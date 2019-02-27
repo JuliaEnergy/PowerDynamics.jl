0d49148235455fa6dc7eb1fd58c5a99243504d60
SabineAuer <sabine.auer@elena-international.com>
2019-02-27 11:55:59 +0100

    fixing issue #31 with updated node docs #46  (#47)
    * CHANGELOG modified
    
    * more explanation added to docs for node dynamics and naming convention of variables taking into account
    
    * Omega_H removed from keyword arguements
    
    * merged with master 2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,5 +6,6 @@
 * ![bugfix](https://img.shields.io/badge/PD-bugfix-%23d73a4a.svg) [symbolsof is now defined on the class level via the @DynamicNode Macro](https://github.com/JuliaEnergy/PowerDynBase.jl/pull/35)
 * ![bugfix](https://img.shields.io/badge/PD-bugfix-%23d73a4a.svg) & ![enhancement](https://img.shields.io/badge/PD-enhancement-%23a2eeef.svg) [add Julia 1.1. to travis/ci and fixed wrong coverage reporting (thus)](https://github.com/JuliaEnergy/PowerDynBase.jl/pull/38)
 * ![enhancement](https://img.shields.io/badge/PD-enhancement-%23a2eeef.svg) [cleaning .travis.yml](https://github.com/JuliaEnergy/PowerDynBase.jl/pull/39)
+* ![enhancement](https://img.shields.io/badge/PD-enhancement-%23a2eeef.svg) [fixing node docs](https://github.com/JuliaEnergy/PowerDynBase.jl/pull/46)
 * ![miscellaneous](https://img.shields.io/badge/PD-miscellaneous-lightgrey.svg) [add Sabine Auer to AUTHORS](https://github.com/JuliaEnergy/PowerDynBase.jl/pull/45)
 * ![bugfix](https://img.shields.io/badge/PD-bugfix-%23d73a4a.svg) [temporarily pin MbedTLS version to 0.6.6 to fix travis ci issue](https://github.com/JuliaEnergy/PowerDynBase.jl/pull/49)
--- src/NodeDynamics/VoltageSourceInverterVoltagePT1.jl
+++ src/NodeDynamics/VoltageSourceInverterVoltagePT1.jl
@@ -6,13 +6,21 @@ VSIVoltagePT1(;τ_v,τ_P,τ_Q,K_P,K_Q,E_r,P,Q)
 A node type that applies the frequency and voltage droop control to control the frequency and
 voltage dynamics.
 
-Additionally to ``u``, it has the internal dynamic variable ``\omega`` representing
-the frequency of the rotator relative to the grid frequency ``\Omega``, i.e. the
-real frequency ``\omega_r`` of the rotator is given as ``\omega_r = \Omega + \omega``.
+`VSIVoltagePT1` models an inverters as AC voltage source which means the amplitude and
+frequency can defined by the designer (often called grid-forming inverter mode).
+The frequency regulation is assumed to be instantaneous, but the voltage control happens with a delay
+`τ_v` that is represented by a first order filter.
+In addition simple proportional controllers are implemented for frequency and voltage such that the frequency `ω` and
+voltage amplitudes `v` of the inverters are modified depending on the deviations (with respect to a desired value) of the active and reactive powers, respectively.
+ift is assumed that active and reactive power are measured via low pass fileters with time constant `τ_P` and `τ_Q`, respectively.
+
+Hence, additionally to `u`, it has the internal dynamic variables
+* `ω` representing the frequency of the rotator relative to the grid frequency ``Ω``, i.e. the real frequency ``ω_r`` of the rotator is given as ``ω_r = Ω + ω``.
+* `q_m` is the measured reactive power at the grid connection point.
 
 # Keyword Arguments
 - `τ_v`: time constant voltage control delay
-- `τ_p`: time constant active power measurement
+- `τ_P`: time constant active power measurement
 - `τ_Q`: time constant reactive power measurement
 - `K_P`: droop constant frequency droop
 - `K_Q`: droop constant voltage droop
@@ -22,7 +30,7 @@ real frequency ``\omega_r`` of the rotator is given as ``\omega_r = \Omega + \om
 
 
 # Mathematical Representation
-Using `VSIVoltagePT1` for node ``a`` applies the equations
+Using `VSIVoltagePT1` for node ``a`` (according to J. Schiffer et. al., eq. (6)) gives the equations
 ```math
 \dot{\phi}_a=\omega_a\\
  \dot{\omega}_a=\frac{1}{\tau_{P,a}}[-\omega_a-K_{P,a} (\Re\left(u_a \cdot i_a^*\right)-P_{ref,a})]\\
@@ -30,6 +38,8 @@ Using `VSIVoltagePT1` for node ``a`` applies the equations
  \tau_Q \dot{q}_{m,a}=-q_{m,a}+\Im\left(u_a \cdot i_a^*\right)\\
  \dot{u}_a=\dot{v_a}e^{j\phi}+j\omega_a u_a\\
 ```
+In general ``τ_V ≪ τ_P``, assuming ``τ_V = 0`` would then lead to `VSIMinimal`.
+
 """
 @DynamicNode VSIVoltagePT1(τ_v,τ_P,τ_Q,K_P,K_Q,V_r,P,Q) <: OrdinaryNodeDynamics() begin
     @assert τ_v > 0 "time constant voltage droop delay should be >0"
