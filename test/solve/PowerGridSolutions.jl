a4a8b8779414b14c3f504a3af895ff76791bbf28
Jan Lisse <jan.lisse@posteo.de>
2019-05-07 22:50:09 +0200

    Fix variable index lookup

--- test/solve/PowerGridSolutions.jl
+++ test/solve/PowerGridSolutions.jl
@@ -1,24 +1,23 @@
 using PowerDynBase
+import PowerDynBase.symbolsof
+import PowerDynBase.dimension
 using NetworkDynamics
 using LightGraphs
 using Test
-include("../../src/solve/PowerGridSolutions.jl")
-include("../../src/solve/solve.jl")
 
+struct Foo
+end
+
+symbolsof(f::Foo) = [:a, :b, :c]
+dimension(f::Foo) = 3
 
 function tests()
-        g = SimpleGraph(3)
-        add_edge!(g, 1, 2)
-        add_edge!(g, 2, 3)
-        add_edge!(g, 3, 1)
-        nodes = [PQAlgebraic(S=1+im*1), SwingEq(H=1., P=-1, D=1, Ω=50), SwingEq(H=1., P=-1, D=1, Ω=50)]
-        lines = [StaticLine(Y=1.0im) for e in edges(g)]
-        nd = network_dynamics(map(construct_node_dynamics, nodes), map(construct_edge, lines), g)
-        powergrid = PowerGrid(nd, g, nodes, lines)
-
-        x0 = ones(systemsize(powergrid))
-        timespan = (0.,10.)
-        sol = solve(powergrid, x0, timespan)
+        @testset "PowerGridSolution should find proper variable_index" begin
+                idx = PowerDynBase.variable_index([Foo()], 1, :c)
+                @test idx == 3
+                idx = PowerDynBase.variable_index([Foo(),Foo()], 2, :b)
+                @test idx == 5
+        end
 end
 
 tests()
