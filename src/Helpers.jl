6babf55e2ef42b6fae4e4876e1896a653d581202
Jan Lisse <jan.lisse@posteo.de>
2019-05-07 17:20:08 +0200

    fix current sign

--- src/Helpers.jl
+++ src/Helpers.jl
@@ -2,10 +2,10 @@ function total_current(e_s, e_d)
     # Keeping with the convention of negative sign for outging current
     current = 0.0im
     for e in e_s
-        current += e[1] + e[2]*im
+        current -= e[1] + e[2]*im
     end
     for e in e_d
-        current -= e[3] + e[4]*im
+        current += e[3] + e[4]*im
     end
     current
 end
