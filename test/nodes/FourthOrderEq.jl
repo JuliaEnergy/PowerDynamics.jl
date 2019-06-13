73724ee6d9a285e703dcb2de8a4f3283591e5697
Jan Lisse <jan.lisse@posteo.de>
2019-06-13 11:45:57 +0200

    Add more tests

--- test/nodes/FourthOrderEq.jl
+++ test/nodes/FourthOrderEq.jl
@@ -1,6 +1,6 @@
 using Test: @testset, @test
 using SymPy: @syms
-using PowerDynBase: FourthEq, construct_vertex, symbolsof
+using PowerDynBase: FourthOrderEq, construct_vertex, symbolsof
 using LinearAlgebra: I
 
 include("NodeTestBase.jl")
@@ -9,7 +9,7 @@ include("NodeTestBase.jl")
     @syms H  D  Ω  T_d_dash T_q_dash X_q_dash X_d_dash X_d X_q positive=true
     @syms P  E_f real=true
     @syms omega domega theta dtheta real=true
-    fourth_order = FourthEq(H=H, P=P, D=D, Ω=Ω, E_f=E_f, T_d_dash=T_d_dash ,T_q_dash=T_q_dash ,X_q_dash=X_q_dash ,X_d_dash=X_d_dash,X_d=X_d, X_q=X_q)
+    fourth_order = FourthOrderEq(H=H, P=P, D=D, Ω=Ω, E_f=E_f, T_d_dash=T_d_dash ,T_q_dash=T_q_dash ,X_q_dash=X_q_dash ,X_d_dash=X_d_dash,X_d=X_d, X_q=X_q)
     fourth_order_vertex = construct_vertex(fourth_order)
 
     @test symbolsof(fourth_order) == [:u_r, :u_i, :θ, :ω]
