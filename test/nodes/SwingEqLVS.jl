7a9f0a39f583076bc06be342c5dfdbeafb7334dd
Jan Lisse <jan.lisse@posteo.de>
2019-06-18 21:05:50 +0200

    Test assertions

--- test/nodes/SwingEqLVS.jl
+++ test/nodes/SwingEqLVS.jl
@@ -10,8 +10,13 @@ include("NodeTestBase.jl")
     @syms P Ω real=true
     @syms omega domega real=true
     @syms V Γ positive=true
+    @syms H_INVALID D_INVALID Γ_INVALID negative=true
     swing_lvs = SwingEqLVS(H=H, P=P, D=D, Ω=Ω, Γ=Γ, V=V)
     swing_lvs_vertex = construct_vertex(swing_lvs)
+
+    @test_throws AssertionError construct_vertex(SwingEqLVS(H=H_INVALID, P=P, D=D, Ω=Ω, Γ=Γ, V=V))
+    @test_throws AssertionError construct_vertex(SwingEqLVS(H=H, P=P, D=D_INVALID, Ω=Ω, Γ=Γ, V=V))
+    @test_throws AssertionError construct_vertex(SwingEqLVS(H=H, P=P, D=D, Ω=Ω, Γ=Γ_INVALID, V=V))
 
     @test symbolsof(swing_lvs) == [:u_r, :u_i, :ω]
     @test swing_lvs_vertex.mass_matrix == I
