befffa8cae97a856a6e2d2d86df48181e42c44dc
Jan Lisse <jan.lisse@posteo.de>
2019-06-26 16:12:55 +0200

    initial powerdrop code

--- src/simulations/simulations.jl
+++ src/simulations/simulations.jl
@@ -13,6 +13,14 @@ end
     to
 end
 
+@Base.kwdef struct PowerDrop
+    percent
+    node_number
+    t1
+    t2
+    t3
+end
+
 struct Inc
     val
 end
@@ -43,12 +51,38 @@ function (p::Perturbation)(op)
     x0
 end
 
+function (pd::PowerDrop)(powergrid)
+    node_list_power_drop = copy(powergrid.nodes)
+    n = powergrid.nodes[pd.node_number]
+    node_list_power_drop[pd.node_number] = SwingEqLVS(
+        H=n.H,
+        P=n.P*pd.percent,
+        D=n.D,
+        Ω=n.Ω,
+        Γ=n.Γ,
+        V=n.V
+    )
+    PowerGrid(powergrid.graph, node_list_power_drop, powergrid.lines)
+end
+
 function simulate(p::Perturbation, powergrid, x0; timespan)
     solve(powergrid, p(x0), timespan);
 end
 
 function simulate(lf::LineFault, powergrid, x0; timespan)
     solve(lf(powergrid), x0, timespan);
+end
+
+function simulate(pd::PowerDrop, powergrid, x0)
+    sol1 = solve(powergrid, x0, pd.t1)
+    final_state1 = sol1(:final)
+
+    g_power_reduction = pd(powergrid)
+    sol2 = solve(g_power_reduction, State(g_power_reduction, final_state1.vec), pd.t2)
+    final_state2 = sol2(:final)
+
+    sol3 = solve(powergrid, State(powergrid, final_state2.vec), pd.t3)
+    sol3
 end
 
 const iipfunc = true # is in-place function
@@ -63,4 +97,5 @@ export Inc
 export Dec
 export LineFault
 export Perturbation
+export PowerDrop
 export simulate
