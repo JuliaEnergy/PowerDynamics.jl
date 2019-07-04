d2cc1fe67f9db794f0277e4701c94fe20b1afc64
Jan Lisse <jan.lisse@posteo.de>
2019-07-04 22:04:59 +0200

    Add tests for all node types

--- test/parsers/JsonParser.jl
+++ test/parsers/JsonParser.jl
@@ -5,11 +5,23 @@ expected_swing_eq = SwingEq(H=1, P=2, D=3, Ω=12)
 expected_swing_eq_lvs = SwingEqLVS(H=1, P=2, D=3, Ω=12, Γ=13, V=14)
 expected_fourth_order_eq = FourthOrderEq(H=1, P=2, D=3, Ω=12, E_f=13, T_d_dash=14 ,T_q_dash=15 ,X_q_dash=16 ,X_d_dash=17,X_d=18, X_q=19)
 expected_slack = SlackAlgebraic(U=10)
+expected_pq = PQAlgebraic(S=10im)
+expected_pv = PVAlgebraic(P=10, V=120)
+expected_vsi_minimal = VSIMinimal(τ_P=1,τ_Q=2,K_P=3,K_Q=4,V_r=5,P=6,Q=7)
+expected_vsi_voltage_pt1 = VSIVoltagePT1(τ_v=1,τ_P=2,τ_Q=3,K_P=4,K_Q=5,V_r=6,P=7,Q=8)
+expected_csi_minimal = CSIMinimal(I_r=1)
+expected_exponential_recov_load = ExponentialRecoveryLoad(P0=1, Q0=2, Nps=3, Npt=4, Nqs=5, Nqt=6, Tp=7, Tq=8, V0=9)
+expected_fourth_order_eq_avr = FourthOrderEqGovernorExciterAVR(
+H=1, P=2, D=3, Ω=4, T_d_dash=5 ,T_q_dash=6,
+X_q_dash=7 ,X_d_dash=8,X_d=9, X_q=10, T_e=11, T_a=12, T_f=13, K_e=14, K_a=15,
+K_f=16, V_ref=17, R_d=18, T_sv=19, T_ch=20)
 
 expected_static_line = StaticLine(from=1, to=2, Y=0.1+5im)
 expected_pimodel_line = PiModelLine(from=1, to=3, y=0.1+5im, y_shunt_km=200, y_shunt_mk=300)
 
-expected_nodes  = [expected_swing_eq, expected_swing_eq_lvs, expected_fourth_order_eq, expected_slack]
+expected_nodes  = [expected_swing_eq, expected_swing_eq_lvs, expected_fourth_order_eq, expected_slack,
+expected_pq, expected_pv, expected_vsi_minimal, expected_vsi_voltage_pt1, expected_csi_minimal,
+expected_exponential_recov_load, expected_fourth_order_eq_avr]
 expected_lines = [expected_static_line, expected_pimodel_line]
 
 grid = read(joinpath(@__DIR__, "grid.json"), Json)
