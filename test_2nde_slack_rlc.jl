3b083fb2bc76948099a6e606f8b219378d0e3222
Jan Lisse <jan.lisse@posteo.de>
2019-07-17 15:31:41 +0200

    Simplify

--- test_2nde_slack_rlc.jl
+++ test_2nde_slack_rlc.jl
@@ -1,27 +1,16 @@
 #using Pkg
 #Pkg.activate(".")
 using PowerDynamics
-using LightGraphs: SimpleGraph, add_edge!, add_vertices!, Edge
-using MetaGraphs: MetaGraph, set_prop!
-
 
 admittance = 1im/0.2 #+ 0.03
 
-
-graph = MetaGraph(SimpleGraph())
 node_list = []
 append!(node_list, [SlackAlgebraic(U=1)])
 append!(node_list, [RLCLoad(R=0.5,L=0.5,C=0.5)])
-num_nodes = length(node_list)
-add_vertices!(graph, num_nodes)
-[set_prop!(graph, n, :node, node_list[n]) for n=1:num_nodes]
 
-line_list =[]
-line = StaticLine(Y=admittance)
-push!(line_list, line)
-add_edge!(graph, 1, 2);
-set_prop!(graph, Edge(1, 2), :line, line)
-powergrid = PowerGrid(graph, node_list, line_list)
+
+line_list =[StaticLine(Y=admittance, from=1, to=2)]
+powergrid = PowerGrid(node_list, line_list)
 
 operationpoint = find_operationpoint(powergrid)
 
