59e2c49b0b4d6adc10604c651f7d9d28b10f1705
Jan Lisse <jan.lisse@posteo.de>
2019-07-17 12:54:25 +0200

    add test

---
+++ test/nodes/RLCLoad.jl
@@ -0,0 +1,16 @@
+using Test: @testset, @test
+using SymPy: @syms
+using PowerDynamics: RLCLoad, construct_vertex, symbolsof
+
+include("NodeTestBase.jl")
+
+@testset "RLCLoad" begin
+    @syms R,L,C real=true
+
+    pv = RLCLoad(R=R, L=L, C=C)
+    pv_vertex = construct_vertex(pv)
+    @test symbolsof(pv) == [:u_r, :u_i, :u_C, :i_L]
+    @test pv_vertex.mass_matrix == [0,0,1,1,1]
+
+    smoketest_rhs(pv_vertex)
+end
