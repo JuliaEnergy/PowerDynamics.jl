29d91f9a1c642f939a6ea1fd4dc52c8f4ee66522
Sabine Auer <auer@pik-potsdam.de>
2019-07-17 12:50:38 +0200

    changes RLCLoad

--- src/nodes/RLCLoad.jl
+++ src/nodes/RLCLoad.jl
@@ -23,17 +23,18 @@ Neville Watson and Jos Arrillaga, IET 2007, p.59, eq. (3.47)
 
 
 """
-@DynamicNode RLCLoad(R,L,C) <: OrdinaryNodeDynamics()  begin
-MassMatrix(m_int = [true, true])
+@DynamicNode RLCLoad(R,L,C)  begin
+MassMatrix(m_int = [true, true,true])
 end begin
     @assert R > 0 "Resistance should be >0"
     @assert L > 0 "Inductance should be >0"
     @assert C > 0 "Capacitance should be >0"
 
-end [[u_C, du_C],[i_L,di_L]] begin
-    du_C = 1/C *i_L
-    di_L = R/L*i_L+1/L*u
+end [[u_C, du_C],[i_L,di_L],[ω,dω]] begin
+    du_C = 1/C *i_L + 1*im*ω*u_c
+    di_L = R/L*i_L+1/L*u + 1*im*ω*i_L
     du = i_L - i # = enforcing current conservation
+    dω = 0
 end
 
 export RLCLoad
