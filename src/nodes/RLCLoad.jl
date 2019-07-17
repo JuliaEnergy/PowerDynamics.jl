62b83eeeb93c8dfd85f6036aebcb7657a89d56a1
Jan Lisse <jan.lisse@posteo.de>
2019-07-17 10:24:34 +0200

    fix error

--- src/nodes/RLCLoad.jl
+++ src/nodes/RLCLoad.jl
@@ -20,10 +20,8 @@ Neville Watson and Jos Arrillaga, IET 2007, p.59, eq. (3.47)
 	\dfrac{du_C}{dt} = \frac{1}{C}i_L(t)\\
     \dfrac{di_L}{dt} = -\frac{R}{L} i_L(t)+\frac{1}{L} u(t)
 ```
-
-
 """
-@DynamicNode RLCLoad(R,L,C) <: OrdinaryNodeDynamics()  begin
+@DynamicNode RLCLoad(R,L,C) begin
 MassMatrix(m_int = [true, true])
 end begin
     @assert R > 0 "Resistance should be >0"
