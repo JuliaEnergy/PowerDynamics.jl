a9595ff3046da98a8fd254f3ae4e5880677861ce
Paul Schultz <pschultz@pik-potsdam.de>
2019-07-25 00:16:45 +0200

    added powerdrop perturbation problem: node types are immutable

--- src/simulations/simulations.jl
+++ src/simulations/simulations.jl
@@ -15,15 +15,12 @@ end
 
 @Base.kwdef struct PowerDrop
     node_number
-    percent
-    t_prefault
+    fraction
     t_fault
-    t_postfault
+    t_clearing
 end
 
 @Base.kwdef struct SinglePhaseShortCircuitToGround
-    #ToDo use from/to and automatically find the line
-    # what is the indexing of lines?
     from
     to
     line_fraction
@@ -67,15 +64,27 @@ function (pd::PowerDrop)(powergrid)
     # PROBLEM: node types are immutable. how to create a copy?
     # fieldnames(::NodeType) is not defined
     node_list_power_drop = copy(powergrid.nodes)
-    node_list_power_drop[pd.node_number].P *= pd.percent
+    node_list_power_drop[pd.node_number].P *= pd.fraction
     PowerGrid(powergrid.graph, node_list_power_drop, powergrid.lines)
 end
 
 function (sc::SinglePhaseShortCircuitToGround)(powergrid)
 
     line_number = findfirst([e==Edge(sc.from, sc.to) for e in edges(powergrid.graph)])
-    line_list_power_drop = copy(powergrid.lines)
-    healthy_line = line_list_power_drop[line_number]
+
+    # this causes
+    #
+    # ERROR: MethodError: Cannot `convert` an object of type PiModelLine to an object of type StaticLine
+    # Closest candidates are:
+    #   convert(::Type{T}, ::T) where T at essentials.jl:154
+    #line_list = copy(powergrid.lines)
+
+    line_list = []
+    for line in powergrid.lines
+        push!(line_list, line)
+    end
+
+    healthy_line = line_list[line_number]
 
     X = inv(healthy_line.Y)
     Xg = inv(sc.short_circuit_admittance)
@@ -84,9 +93,9 @@ function (sc::SinglePhaseShortCircuitToGround)(powergrid)
     Xl_shunt = inv(1. - sc.line_fraction) * Xg + sc.line_fraction * X
     Xr_shunt = inv(sc.line_fraction) * Xg + (1. - sc.line_fraction) * X
 
-    line_list_power_drop[line_number] = PiModelLine(y=inv(Xprime), y_shunt_km=inv(Xl_shunt), y_shunt_mk=inv(Xr_shunt))
+    line_list[line_number] = PiModelLine(from=sc.from, to = sc.to, y=inv(Xprime), y_shunt_km=inv(Xl_shunt), y_shunt_mk=inv(Xr_shunt))
 
-    PowerGrid(powergrid.graph, powergrid.nodes, line_list_power_drop)
+    PowerGrid(powergrid.graph, powergrid.nodes, line_list)
 end
 
 const iipfunc = true # is in-place function
@@ -99,31 +108,18 @@ function simulate(lf::LineFault, powergrid, x0; timespan)
     solve(lf(powergrid), x0, timespan);
 end
 
-# function simulate(pd::PowerDrop, powergrid, x0)
-#     sol1 = solve(powergrid, x0, pd.t_prefault)
-#     final_state1 = sol1(:final)
-#
-#     g_power_reduction = pd(powergrid)
-#     sol2 = solve(g_power_reduction, State(g_power_reduction, final_state1.vec), pd.t_fault)
-#     final_state2 = sol2(:final)
-#
-#     sol3 = solve(powergrid, State(powergrid, final_state2.vec), pd.t_postfault)
-#
-#     CompositePowerGridSolution([sol1, sol2, sol3], [powergrid, g_power_reduction, powergrid])
-# end
+function simulate(pd::PowerDrop, powergrid, x0, timespan)
+    @assert first(timespan) <= pd.t_fault "fault cannot begin in the past"
+    @assert pd.t_clearing <= last(timespan) "fault cannot end in the future"
 
-function simulate(sc::SinglePhaseShortCircuitToGround, powergrid, x0; timespan)
-    @assert first(timespan) <= sc.t_fault "fault cannot begin in the past"
-    @assert sc.t_clearing <= last(timespan) "fault cannot end in the future"
-
-    problem = ODEProblem{iipfunc}(ode_function(sc(powergrid)), x0.vec, (first(timespan), sc.t_clearing))
+    problem = ODEProblem{iipfunc}(rhs(pd(powergrid)), x0.vec, (first(timespan), pd.t_clearing))
     fault_integrator = init(problem, Rodas4(autodiff=false))
 
-    reinit!(fault_integrator, x0.vec, t0=sc.t_fault, tf=sc.t_clearing, erase_sol=false)
-    #savevalues!(fault_integrator)
+    reinit!(fault_integrator, x0.vec, t0=pd.t_fault, tf=pd.t_clearing, erase_sol=false)
+    savevalues!(fault_integrator)
     solve!(fault_integrator)
 
-    problem = ODEProblem{iipfunc}(ode_function(powergrid), fault_integrator.u, (sc.t_clearing, last(timespan)))
+    problem = ODEProblem{iipfunc}(rhs(powergrid), fault_integrator.u, (pd.t_clearing, last(timespan)))
     integrator = init(problem, Rodas4(autodiff=false))
 
     # Now the trick: copy solution object to new integrator and
@@ -138,16 +134,38 @@ function simulate(sc::SinglePhaseShortCircuitToGround, powergrid, x0; timespan)
 
     return PowerGridSolution(integrator.sol, powergrid)
 
-    # sol1 = solve(powergrid, x0, sc.t_prefault)
-    # final_state1 = sol1(:final)
-    #
-    # g_faulted_line = sc(powergrid)
-    # sol2 = solve(g_faulted_line, State(g_faulted_line, final_state1.vec), sc.t_fault)
-    # final_state2 = sol2(:final)
-    #
-    # sol3 = solve(powergrid, State(powergrid, final_state2.vec), sc.t_postfault)
-    #
-    # CompositePowerGridSolution([sol1, sol2, sol3], [powergrid, g_faulted_line, powergrid])
+
+end
+
+simulate(pd::PowerDrop, powergrid, x0) = simulate(pd, powergrid, x0, (0., pd.t_clearing))
+
+
+
+function simulate(sc::SinglePhaseShortCircuitToGround, powergrid, x0; timespan)
+    @assert first(timespan) <= sc.t_fault "fault cannot begin in the past"
+    @assert sc.t_clearing <= last(timespan) "fault cannot end in the future"
+
+    problem = ODEProblem{iipfunc}(rhs(sc(powergrid)), x0.vec, (first(timespan), sc.t_clearing))
+    fault_integrator = init(problem, Rodas4(autodiff=false))
+
+    reinit!(fault_integrator, x0.vec, t0=sc.t_fault, tf=sc.t_clearing, erase_sol=false)
+    savevalues!(fault_integrator)
+    solve!(fault_integrator)
+
+    problem = ODEProblem{iipfunc}(rhs(powergrid), fault_integrator.u, (sc.t_clearing, last(timespan)))
+    integrator = init(problem, Rodas4(autodiff=false))
+
+    # Now the trick: copy solution object to new integrator and
+    # make sure the counters are updated, otherwise sol is overwritten in the
+    # next step.
+    integrator.sol = fault_integrator.sol
+    integrator.saveiter = fault_integrator.saveiter
+    integrator.saveiter_dense = fault_integrator.saveiter_dense
+    integrator.success_iter = fault_integrator.success_iter
+
+    solve!(integrator)
+
+    return PowerGridSolution(integrator.sol, powergrid)
 end
 
 function solve(pg::PowerGrid, x0, timespan)
--- test/simulations/simulations.jl
+++ test/simulations/simulations.jl
@@ -27,6 +27,23 @@ faulty_grid = line_fault(grid)
 @test length(faulty_grid.lines) == 1
 @test collect(edges(faulty_grid.graph)) == [Edge(1,3)]
 
+@testset "test SinglePhaseShortCircuitToGround simulation" begin
+    nodes = [SwingEqLVS(H=1., P=-1, D=1, Ω=50, Γ=20, V=1), SwingEqLVS(H=1., P=-1, D=1, Ω=50, Γ=20, V=1)]
+    graph = SimpleGraph(2)
+    add_edge!(graph, 1, 2);
+    lines = [StaticLine(Y=-im)]
+    grid = PowerGrid(graph, nodes, lines)
+    state = State(grid, rand(systemsize(grid)))
+
+    sol = simulate(SinglePhaseShortCircuitToGround(
+        from=1,
+        to=2,
+        line_fraction=0.99,
+        short_circuit_admittance=1e6,
+        t_fault=.1,
+        t_clearing=.5)
+    @test sol != nothing
+end
 
 @testset "test PowerDrop simulation" begin
     nodes = [SwingEqLVS(H=1., P=-1, D=1, Ω=50, Γ=20, V=1), SwingEqLVS(H=1., P=-1, D=1, Ω=50, Γ=20, V=1)]
@@ -37,10 +54,10 @@ faulty_grid = line_fault(grid)
     state = State(grid, rand(systemsize(grid)))
 
     sol = simulate(PowerDrop(
-    percent = 0.9,
-    node_number = 1,
-    t1 = (0., 2.),
-    t2 = (2., 3.),
-    t3 = (3., 5.)), grid, state)
+        fraction = 0.9,
+        node_number = 1,
+        t_fault = 2.,
+        t_clearing=3.),
+        grid, state, (0., 5.))
     @test sol != nothing
 end
