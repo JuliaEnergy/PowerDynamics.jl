f711c1c6736c2d79541e5f8ee3c30c0c25be8629
janlisse <jan.lisse@posteo.de>
2019-08-08 17:17:27 +0200

    Rlc load (#35)
    * work on RLC load
    
    * RLCLoad.jl draft file added
    
    * fix error
    
    * changes RLCLoad
    
    * add test
    
    * fix test
    
    * add omega
    
    * RLCLoad checked and two node test with slack added
    
    * Fix showdefinition output (catch block wasn't shown)
    
    * include command for PiModel.jl and Transformer.jl added (#29)
    
    * include command for PiModel.jl and Transformer.jl added
    
    * CHANGELOG.md udpated
    
    * SparseArrays dependy added
    
    * 2.1.0 (#30)
    
    * Parsing of json format & getting rid of Metagraphs
    
    * Implement export to Json
    
    * Add tests for all node types
    
    * Fix naming & exports
    
    * Remove obsolete dep.
    
    * Get rid of CSVParser, since it is not generic enough
    
    * Remove comment
    
    * Narrow DiffEq deps.
    
    * Improve tests
    
    * include command for PiModel.jl and Transformer.jl added (#29)
    
    * include command for PiModel.jl and Transformer.jl added
    
    * CHANGELOG.md udpated
    
    * SparseArrays dependy added
    
    * No need for SparseArrays here (and additional dep.)
    
    * Fix test
    
    * Update Project.toml
    
    Prepare next release
    
    * Update PiModel.jl
    
    sparsearrays no longer needed
    
    * changes RLCLoad
    
    * add test
    
    * fix merge error
    
    * Simplify
    
    * fix rlc smoketest
    
    * cleanup
    
    * try to fix hanging build
    
    * drop builds for julia 1.0

---
+++ src/nodes/RLCLoad.jl
@@ -0,0 +1,38 @@
+# (C) 2018 Potsdam Institute for Climate Impact Research, authors and contributors (see AUTHORS file)
+# Licensed under GNU GPL v3 (see LICENSE file)
+
+@doc doc"""
+```Julia
+RLCLoad(R,L,C)
+```
+A node type that represents the RLC load model according to
+"Power Systems Electromagnetic Transients Simulation",
+Neville Watson and Jos Arrillaga, IET 2007, p.59, eq. (3.47)
+
+# Keyword Arguments
+- `R`: resistance in [?]
+- `L`: inductance in [?]
+- `C`: capacitance in [?]
+
+
+# Mathematical Representation
+```math
+	\dfrac{du_C}{dt} = \frac{1}{C}i_L(t)\\
+    \dfrac{di_L}{dt} = -\frac{R}{L} i_L(t)+\frac{1}{L} u(t)
+```
+"""
+@DynamicNode RLCLoad(R,L,C)  begin
+MassMatrix(m_int = [true, true,true])
+end begin
+    @assert R > 0 "Resistance should be >0"
+    @assert L > 0 "Inductance should be >0"
+    @assert C > 0 "Capacitance should be >0"
+
+end [[u_C, du_C],[i_L,di_L],[ω,dω]] begin
+    du_C = 1/C *i_L + 1*im*ω*u_C
+    di_L = -R/L*i_L+1/L*u -1/L*u_C+ 1*im*ω*i_L
+    du = i_L - i # = enforcing current conservation
+    dω = 0
+end
+
+export RLCLoad
