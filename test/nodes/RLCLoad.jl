9c1262acff92618de0e941df4f06aa117d034ee4
Jan Lisse <jan.lisse@posteo.de>
2019-08-08 15:15:23 +0200

    fix rlc smoketest

--- test/nodes/RLCLoad.jl
+++ test/nodes/RLCLoad.jl
@@ -6,11 +6,12 @@ include("NodeTestBase.jl")
 
 @testset "RLCLoad" begin
     @syms R L C positive=true
+    @syms u_C i_L omega domega du_C di_L real=true
 
     pv = RLCLoad(R=R, L=L, C=C)
     pv_vertex = construct_vertex(pv)
     @test symbolsof(pv) == [:u_r, :u_i, :u_C, :i_L, :ω]
     @test pv_vertex.mass_matrix == [0,0,1,1,1]
 
-    smoketest_rhs(pv_vertex)
+    smoketest_rhs(pv_vertex, int_x=[u_C, i_L, omega], int_dx=[domega, du_C, di_L])
 end
