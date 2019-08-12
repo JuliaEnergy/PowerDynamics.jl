62dbd0153acdcf5a56cf564171124747464f6f8d
Sabine Auer <auer@pik-potsdam.de>
2019-08-12 11:43:54 +0200

    testing of new node

--- src/nodes/PVInverterWithFrequencyControl.jl
+++ src/nodes/PVInverterWithFrequencyControl.jl
@@ -40,25 +40,32 @@ Using `VSIMinimal` for node ``a`` (according to J. Schiffer et. al., eq. (7)) gi
 ```
 """
 @DynamicNode PVInverterWithFrequencyControl(I_n,k_PLL,f,f_s,T_m,k_P) begin
+    MassMatrix()
+end  begin
+    # no prep
 end [[θ_PLL,dθ_PLL],[v_xm,dv_xm],[v_ym,dv_ym],[P,dP]] begin
     v_x = real(u)
     v_y = imag(u)
-    #i_x = real(i)
-    #i_y = imag(i)
     p = real(u * conj(i))
+    I_P = I_n
+    I_Q = 0
 
     dv_xm = 1/T_m*(v_x-v_xm)
     dv_ym = 1/T_m*(v_y-v_ym)
 
-    v_q = v_xm*cosθ_PLL+v_ym*sinθ_PLL
-    v_d = -v_xm*sinθ_PLL+v_ym*cosθ_PLL
+    v_q = v_xm*cos(θ_PLL)+v_ym*sin(θ_PLL)
+    v_d = -v_xm*sin(θ_PLL)+v_ym*cos(θ_PLL)
 
     dθ_PLL = v_q*(-k_PLL)
 
     f_m = (1+dθ_PLL)*f
-    P = I_n*v_d
+    P = I_P*v_d
     dP = -k_P*(f_m-f_s)/f*p
-    du = u*1im*2π*dθ_PLL*f
+
+    i_x = I_P*cos(θ_PLL) + I_Q*sin(θ_PLL)
+    i_y = I_P*sin(θ_PLL) - I_Q*cos(θ_PLL)
+
+    du = i-(i_x+1im*i_y)
 end
 
 export PVInverterWithFrequencyControl
--- test_PVInverter.jl
+++ test_PVInverter.jl
@@ -5,13 +5,19 @@ using PowerDynamics
 node_list=[]
 
 append!(node_list,[SlackAlgebraic(U=1.)])
-append!(node_list,[PVInverterWithFrequencyControl(I_n=1.,k_PLL=0.1,f=50,f_s=50.01,T_m=0.2,k_P=0.2)])
+append!(node_list, [SwingEqLVS(H=1, P=1, D=0.01, Ω=50,Γ=0.1,V=1.)])
+append!(node_list,[PVInverterWithFrequencyControl(I_n=1.,k_PLL=0.1,f=50,f_s=50,T_m=0.2,k_P=0.2)])
 
 line_list=[]
 append!(line_list,[StaticLine(from=1,to=2,Y=-1im/0.2)])
+append!(line_list,[StaticLine(from=2,to=3,Y=-1im/0.2)])
+
 
 powergrid = PowerGrid(node_list,line_list)
 
 operationpoint = find_operationpoint(powergrid)
 
-#result = simulate(Perturbation(2, :ω, Inc(0.5)), powergrid, operationpoint, timespan = (0.0,1.))
+result = simulate(Perturbation(2, :ω, Inc(0.5)), powergrid, operationpoint, timespan = (0.0,1.))
+
+include("plotting.jl")
+plot_res(result,powergrid,2)
