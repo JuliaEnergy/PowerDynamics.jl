242154bcc9813de0efc9d8446273daa1ccc63e9f
Sabine Auer <auer@pik-potsdam.de>
2019-08-13 18:31:05 +0200

    PV inverter changed, fix point search unstable

--- src/nodes/PVInverterWithFrequencyControl.jl
+++ src/nodes/PVInverterWithFrequencyControl.jl
@@ -7,7 +7,7 @@ TODO docs
 
 """
 @DynamicNode PVInverterWithFrequencyControl(I_n,k_PLL,f,f_s,T_m,k_P) begin
-    MassMatrix(m_int = [true,true,true,true])
+    MassMatrix(m_u = true,m_int = [true,true,true,false])
 end  begin
     # no prep
 end [[θ_PLL,dθ_PLL],[v_xm,dv_xm],[v_ym,dv_ym],[P,dP]] begin
@@ -29,15 +29,13 @@ end [[θ_PLL,dθ_PLL],[v_xm,dv_xm],[v_ym,dv_ym],[P,dP]] begin
     end
 
     f_m = (1+dθ_PLL)*f
-    P = I_P*v_d
-    dP = -k_P*(f_m-f_s)/f*p
+    P = I_P*v_d -k_P*(f_m-f_s)/f*p
+    dP = p-P
     #println(P)
     println(v_q)
     i_x = I_P*cos(θ_PLL) + I_Q*sin(θ_PLL)
     i_y = I_P*sin(θ_PLL) - I_Q*cos(θ_PLL)
-
-    du = i-(i_x+1im*i_y)
-    #du = P-p
+    du = u*1im*dθ_PLL
 end
 
 export PVInverterWithFrequencyControl
