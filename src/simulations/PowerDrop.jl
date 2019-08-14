142376e848bb64869ac8cfc096f54194ac0e82a3
Jan Lisse <jan.lisse@posteo.de>
2019-08-14 10:25:33 +0200

    Add old solution for comparison

--- src/simulations/PowerDrop.jl
+++ src/simulations/PowerDrop.jl
@@ -13,7 +13,7 @@ function (pd::PowerDrop)(powergrid)
     node_for_drop = node_list_power_drop[pd.node_number]
     node_for_drop = @set node_for_drop.P *= pd.fraction
     node_list_power_drop[pd.node_number] = node_for_drop
-    PowerGrid(powergrid.graph, node_list_power_drop, powergrid.lines)
+    PowerGrid(node_list_power_drop, powergrid.lines)
 end
 
 function simulate(pd::PowerDrop, powergrid, x0, timespan)
@@ -38,5 +38,22 @@ function simulate(pd::PowerDrop, powergrid, x0, timespan)
     return PowerGridSolution(integrator.sol, powergrid)
 end
 
+function simulate2(pd::PowerDrop, powergrid, x0, timespan)
+    @assert first(timespan) <= pd.tspan_fault[1] "fault cannot begin in the past"
+    @assert pd.tspan_fault[2] <= last(timespan) "fault cannot end in the future"
+
+    sol1 = solve(powergrid, x0, (timespan[1], pd.tspan_fault[1]))
+
+    final_state1 = sol1(:final)
+    powergrid_drop = pd(powergrid)
+    sol2 = solve(powergrid_drop, final_state1, (pd.tspan_fault[1], pd.tspan_fault[2]))
+
+    final_state2 = sol2(:final)
+    # solve after clearance of the fault
+    sol3 = solve(powergrid, final_state2, (pd.tspan_fault[2], last(timespan)))
+
+    return (sol1, sol2, sol3)
+end
+
 export PowerDrop
-export simulate
+export simulate, simulate2
