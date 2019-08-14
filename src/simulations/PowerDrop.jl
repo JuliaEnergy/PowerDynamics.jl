4462db21382457a09a61e04175dc870a0404fc10
Jan Lisse <jan.lisse@posteo.de>
2019-08-14 17:57:20 +0200

    Add back Pauls original solution, this works!

--- src/simulations/PowerDrop.jl
+++ src/simulations/PowerDrop.jl
@@ -55,5 +55,34 @@ function simulate2(pd::PowerDrop, powergrid, x0, timespan)
     return (sol1, sol2, sol3)
 end
 
+function simulate3(pd::PowerDrop, powergrid, x0, timespan)
+    @assert first(timespan) <= pd.tspan_fault[1] "fault cannot begin in the past"
+    @assert pd.tspan_fault[2] <= last(timespan) "fault cannot end in the future"
+
+    problem = ODEProblem{iipfunc}(rhs(pd(powergrid)), x0.vec, (first(timespan), pd.tspan_fault[2]))
+    fault_integrator = init(problem, Rodas4(autodiff=false))
+
+    reinit!(fault_integrator, x0.vec, t0=pd.tspan_fault[1], tf=pd.tspan_fault[2], erase_sol=false)
+    savevalues!(fault_integrator)
+    solve!(fault_integrator)
+
+    problem = ODEProblem{iipfunc}(rhs(powergrid), fault_integrator.u, (pd.tspan_fault[2], last(timespan)))
+    integrator = init(problem, Rodas4(autodiff=false))
+
+    # Now the trick: copy solution object to new integrator and
+    # make sure the counters are updated, otherwise sol is overwritten in the
+    # next step.
+    integrator.sol = fault_integrator.sol
+    integrator.saveiter = fault_integrator.saveiter
+    integrator.saveiter_dense = fault_integrator.saveiter_dense
+    integrator.success_iter = fault_integrator.success_iter
+
+    solve!(integrator)
+
+    return PowerGridSolution(integrator.sol, powergrid)
+
+
+end
+
 export PowerDrop
-export simulate, simulate2
+export simulate, simulate2, simulate3
