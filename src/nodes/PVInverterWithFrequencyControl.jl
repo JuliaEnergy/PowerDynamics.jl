90faab203f96e7efb4701268663874fff800abda
Sabine Auer <auer@pik-potsdam.de>
2019-08-14 21:34:13 +0200

    debugging, WIP

--- src/nodes/PVInverterWithFrequencyControl.jl
+++ src/nodes/PVInverterWithFrequencyControl.jl
@@ -14,8 +14,6 @@ end [[θ_PLL,dθ_PLL],[v_xm,dv_xm],[v_ym,dv_ym],[P,dP]] begin
     v_x = real(u)
     v_y = imag(u)
     p = real(u * conj(i))
-    I_P = I_n
-    I_Q = 0
 
     dv_xm = 1/T_m*(v_x-v_xm)
     dv_ym = 1/T_m*(v_y-v_ym)
@@ -24,8 +22,9 @@ end [[θ_PLL,dθ_PLL],[v_xm,dv_xm],[v_ym,dv_ym],[P,dP]] begin
     println("v_xm: ",v_xm)
     println("v_ym: ",v_ym)
 
+
     v_d = v_xm*cos(θ_PLL)+v_ym*sin(θ_PLL)
-    v_q = -v_xm*sin(θ_PLL)+v_ym*cos(θ_PLL)
+    v_q = v_xm*sin(θ_PLL)-v_ym*cos(θ_PLL)
 
     println("v_d: ",v_d)
     println("v_q: ",v_q)
@@ -35,18 +34,21 @@ end [[θ_PLL,dθ_PLL],[v_xm,dv_xm],[v_ym,dv_ym],[P,dP]] begin
         stop()
     end
 
+
     f_m = (1+dθ_PLL)*f
     ω = dθ_PLL*f*2π
     println("P: ",P)
     println("p: ",p)
-    println("I_P*v_d: ",I_P*v_d)
-    P = I_P*v_d -k_P*(f_m-f_s)/f*p
-    i_x = I_P*cos(θ_PLL) - I_Q*sin(θ_PLL)
-    i_y = I_P*sin(θ_PLL) + I_Q*cos(θ_PLL)
+    #P=p
+    dP =-k_P*(f_m-f_s)/f*p
+    I_P = P/v_d
+    println("I_P: ",I_P)
+    I_Q = 0.
+    i_x = I_P*cos(θ_PLL) + I_Q*sin(θ_PLL)
+    i_y = I_P*sin(θ_PLL) - I_Q*cos(θ_PLL)
     println("abs(i_x+1im*i_y): ",i_x+1im*i_y)
     println("i: ",i)
     du = i-(i_x+1im*i_y)
-    #dP = p-P
     #du = u*1im*ω
 end
 
