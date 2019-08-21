063606fe1e02479d32e4233b1423f3e476f6f38a
janlisse <jan.lisse@posteo.de>
2019-08-21 11:08:45 +0200

    Allow init code block for line types (#44)

--- src/lines/LineMacro.jl
+++ src/lines/LineMacro.jl
@@ -1,16 +1,23 @@
 using MacroTools
 
-macro Line(typedef, func_body)
-    return create(typedef, func_body)
+macro Line(typedef, prep, func_body)
+    return create(typedef, prep, func_body)
 end
 
-function create_line(name, parameters, func_body)
+macro Line(typedef, func_body)
+    return create(typedef, nothing, func_body)
+end
+
+function create_line(name, prep, parameters, func_body)
     struct_exp = create_struct(name, parameters)
 
     ce_call = :(construct_edge(par::$(name)))
     extracted_parameters = map(sym -> :( $sym = par.$sym ), parameters)
     cl_body = quote end
     append!(cl_body.args, extracted_parameters)
+    if (prep !== nothing)
+        append!(cl_body.args, prep.args)
+    end
     cl_function = Expr(:function, ce_call, cl_body)
 
     rhscall = :(rhs!(e,v_s,v_d,p,t))
@@ -56,9 +63,9 @@ function create_showdefinition(exp, name)
     return :(showdefinition(io::IO, ::Type{$name}) = println(io, $mainexstr))
 end
 
-function create(typedef, func_body)
+function create(typedef, prep, func_body)
     @capture(typedef, name_(parameters__))
-    mainex = create_line(name, parameters, func_body)
+    mainex = create_line(name, prep, parameters, func_body)
     showex = create_showdefinition(mainex, name)
     append!(mainex.args, [showex])
     return esc(mainex)
