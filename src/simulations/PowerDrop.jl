a368d6919c01d89850cd519129da18da6f28be28
Jan Lisse <jan.lisse@posteo.de>
2019-08-28 10:41:09 +0200

    cleanup & prepare for mergin into master

--- src/simulations/PowerDrop.jl
+++ src/simulations/PowerDrop.jl
@@ -16,49 +16,8 @@ function (pd::PowerDrop)(powergrid)
     PowerGrid(node_list_power_drop, powergrid.lines)
 end
 
-# this doesn't work & hangs in an infinite loop
-function simulate(pd::PowerDrop, powergrid, x0, timespan)
-    @assert first(timespan) <= pd.tspan_fault[1] "fault cannot begin in the past"
-    @assert pd.tspan_fault[2] <= last(timespan) "fault cannot end in the future"
-
-    problem = ODEProblem{true}(rhs(powergrid), x0.vec, timespan)
-    integrator = init(problem, Rodas4(autodiff=false))
-
-    step!(integrator, pd.tspan_fault[1], true)
-
-    # update integrator with error
-    integrator.f = rhs(pd(powergrid))
-
-    step!(integrator, pd.tspan_fault[2], true)
-
-    # update integrator, clear error
-    integrator.f = rhs(powergrid)
-
-    solve!(integrator)
-
-    return PowerGridSolution(integrator.sol, powergrid)
-end
-
-# this works, but leads to 3 seaprate solutions that need to be combined later
-function simulate2(pd::PowerDrop, powergrid, x0, timespan)
-    @assert first(timespan) <= pd.tspan_fault[1] "fault cannot begin in the past"
-    @assert pd.tspan_fault[2] <= last(timespan) "fault cannot end in the future"
-
-    sol1 = solve(powergrid, x0, (timespan[1], pd.tspan_fault[1]))
-
-    final_state1 = sol1(:final)
-    powergrid_drop = pd(powergrid)
-    sol2 = solve(powergrid_drop, final_state1, (pd.tspan_fault[1], pd.tspan_fault[2]))
-
-    final_state2 = sol2(:final)
-    # solve after clearance of the fault
-    sol3 = solve(powergrid, final_state2, (pd.tspan_fault[2], last(timespan)))
-
-    return (sol1, sol2, sol3)
-end
-
 # this works, but the code looks hacky. Can't we do any better?
-function simulate3(pd::PowerDrop, powergrid, x0, timespan)
+function simulate(pd::PowerDrop, powergrid, x0, timespan)
     @assert first(timespan) <= pd.tspan_fault[1] "fault cannot begin in the past"
     @assert pd.tspan_fault[2] <= last(timespan) "fault cannot end in the future"
 
