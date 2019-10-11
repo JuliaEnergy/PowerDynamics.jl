e6aae1a955705b3a72b281d0864c1600ff243996
Sabine Auer <auer@pik-potsdam.de>
2019-10-11 14:54:22 +0200

    RLC Load debugging

--- test/nodes/experimental/RLCLoad.jl
+++ test/nodes/experimental/RLCLoad.jl
@@ -6,12 +6,15 @@ include("../NodeTestBase.jl")
 
 @testset "RLCLoad" begin
     @syms R L C positive=true
-    @syms u_C i_L omega domega du_C di_L real=true
+    #@syms u_C i_L omega domega du_C di_L real=true
+    @syms x dx real=true
 
     pv = RLCLoad(R=R, L=L, C=C)
     pv_vertex = construct_vertex(pv)
-    @test symbolsof(pv) == [:u_r, :u_i, :u_C, :i_L, :ω]
-    @test pv_vertex.mass_matrix == [0,0,1,1,1]
-
-    smoketest_rhs(pv_vertex, int_x=[u_C, i_L, omega], int_dx=[domega, du_C, di_L])
+    @test symbolsof(pv) == [:u_r,:u_i,:x]
+    #@test symbolsof(pv) == [:u_r, :u_i, :u_C, :i_L, :ω]
+    #@test pv_vertex.mass_matrix == [0,0,1,1,1]
+    @test pv_vertex.mass_matrix == I
+    smoketest_rhs(pv_vertex, int_x=[x], int_dx=[dx])
+    #smoketest_rhs(pv_vertex, int_x=[u_C, i_L, omega], int_dx=[domega, du_C, di_L])
 end
