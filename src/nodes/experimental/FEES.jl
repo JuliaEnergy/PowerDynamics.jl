afd269a175541a8808ab120dcb4718979bb44fbf
Sabine Auer <auer@pik-potsdam.de>
2019-10-17 19:28:49 +0200

    fess continued

--- src/nodes/experimental/FEES.jl
+++ src/nodes/experimental/FEES.jl
@@ -37,8 +37,36 @@ Using `FESS` applies the equations for the stator circuit:
 """
 @DynamicNode FESS() begin
 end  begin
-end [[θ,dθ],[Ψ_d,dΨ_d],[Ψ_q,dΨ_q],[Ψ_D,dΨ_D],[Ψ_Q,dΨ_Q],[Ψ_e,dΨ_e],[ω,dω]] begin
-    
+end [[θ_PLL,dθ_PLL],[i_dm,di_dm],[i_qm,di_qm],[ω_m,dω_m],[v_qm,dv_qm],[i_qm_ref,di_qm_ref],[i_dg,di_dg],[i_qg,di_qg],[u_dc,du_dc],[v_dg,dv_dg],[i_dg_ref,di_dg_ref]] begin
+    v_x = real(u)
+    v_y = imag(u)
+    v_d = v_x
+
+    dθ_PLL = -i_qg*k_PLL
+
+    # simplifications
+    v_dg = L_g*ω*i_qg-u_dc+E
+    v_qg = -L_g*ω*i_dg-u_dc
+    v_dm = L_m*P_n*ω_m*i_qm - u_dc
+    v_qm = -L_m*P_n*ω_m*i_dm - P_n*Ψ_m-u_dc
+
+    #machine-side converter equations:
+    di_dm = 1/L_m*(-R_m*i_dm+v_dm)
+    di_qm = 1/L_m*(-R_m*i_qm+v_qm)
+    dω_m = 3*P_n/(2*J)*Ψ_m*i_qm
+    # PI inner current controller
+    dv_qm = K_m1*(di_qm_ref-di_qm)+K_m2*(i_qm_ref-i_qm)
+    # PI outer speed controller
+    di_qm_ref = K_m3*dω_m+K_m4*(ω_m-ω_m_ref)
+
+    # grid-side converter
+    di_dg = 1/L_g*(-R_g*i_dg+v_dg)
+    di_qg = 1/L_g*(-R_g*i_qm+v_qg)
+    duc = 3/(2*C)*(i_dg+i_dq)
+    # PI inner current controller
+    dv_dg = K_g1*(di_dg_ref-di_dg)+K_g2*(i_dg_ref-i_dg)
+    # PI outer speed controller
+    di_dg_ref = K_g3*(du_dc)+K_m4*(u_dc_ref-u_dc)
 end
 
 export FESS
