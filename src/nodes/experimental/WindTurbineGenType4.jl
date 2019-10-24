a35f98a193650ce0dc28be19c83fdb2c5dad7086
Sabine Auer <sabine.auer@elena-international.com>
2019-10-24 14:23:30 +0200

    correct PI control implementation

--- src/nodes/experimental/WindTurbineGenType4.jl
+++ src/nodes/experimental/WindTurbineGenType4.jl
@@ -5,7 +5,13 @@ WindTurbineGenType4(;I_n,k_PLL,f,f_s,T_m,k_P,τ_ω)
 
 ```
 """
-PI_control(e,u)
+function PI_control(e,u,K_P,K_I)
+    @assert K_P>=0
+    @assert K_I>=0
+    de_I=e
+    u = K_P*e+K_I*e_I
+    return [de_I,u]
+end
 
 @DynamicNode WindTurbineGenType4(K_PLL,Q_ref,C,J,P,ω_rref,u_dcref,K_Q,K_v,K_g1,K_g2,K_r1,K_r2) begin
     MassMatrix(m_u = false,m_int = [true,true,true,true,true,true, true])
@@ -17,29 +23,38 @@ end  begin
     @assert K_r2 >=0
     @assert ω_rref>0
     @assert C>=0
-end [[θ,dθ],[t_e,dt_e],[u_dc,du_dc],[i_dref,di_dref],[i_qref,di_qref],[u_tref,du_tref],[ω_r,dω_r]] begin
+end [[θ,dθ],[e_IP,de_IP],[e_IV,de_IV],[u_dc,du_dc],[i_q,di_q],[u_tref,du_tref],[ω_r,dω_r]] begin
     u_dq = u*exp(-1im*θ)
     v_d = real(u_dq)
     v_q = imag(u_dq)
     dθ = v_q*K_PLL
 
-    t_m = P/ω_r
-    dω_r = 1/J*(t_m-t_e)
-
-    dt_e = K_r1*dω_r+K_r2*(ω_r-ω_rref)
-    p_in = t_e*ω_r
     s_e = u*conj(i)# s=(v_d*i_d+v_q*i_q)+j(v_q*i_d-v_d*i_q)
     p_e = real(s_e)
     q_e = imag(s_e)
 
+    # PI speed control:
+    e_p = -(ω_rref-ω_r)
+    de_Ip = e_p
+    t_e = K_r1*e_p+K_r2*e_IP
+    p_in =t_e/ω_r
+
+    t_m = P/ω_r
+    dω_r = 1/J*(t_m-t_e)
+
     du_dc = 1/C*(p_in-p_e)
 
     u_t=v_d#TODO: discuss!!!
-    di_dref = K_g1*du_dc + K_g2*(u_dc-u_dcref)
-    du_tref = K_Q*(Q_ref-q_e)
-    di_qref = K_v*(u_tref-u_t)
 
-    du = i - (i_dref+1im*i_qref)*exp(1im*θ)
+    # PI voltage control:
+    e_V = -(u_dcref-u_dc)
+    de_IV = e_V
+    i_d = K_g1*e_V + K_g2*de_IV
+
+    du_tref = K_Q*(Q_ref-q_e)
+    di_q = K_v*(u_tref-u_t)
+
+    du = i - (i_d+1im*i_q)*exp(1im*θ)
 end
 
 export WindTurbineGenType4
