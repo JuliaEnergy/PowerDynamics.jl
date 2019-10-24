8b82dac8f9f7ea0ac29cbf6ff68d3caf8963b588
Sabine Auer <sabine.auer@elena-international.com>
2019-10-24 18:44:36 +0200

    tests debugged and wind model is running, plausiblity checks are still a todo

--- src/nodes/experimental/WindTurbineGenType4.jl
+++ src/nodes/experimental/WindTurbineGenType4.jl
@@ -23,37 +23,48 @@ end  begin
     @assert K_r2 >=0
     @assert ω_rref>0
     @assert C>=0
+    @assert K_PLL>=0
 end [[θ,dθ],[e_IP,de_IP],[e_IV,de_IV],[u_dc,du_dc],[i_q,di_q],[u_tref,du_tref],[ω_r,dω_r]] begin
     u_dq = u*exp(-1im*θ)
     v_d = real(u_dq)
     v_q = imag(u_dq)
-    dθ = v_q*K_PLL
+    dθ = v_d*K_PLL
+    ω = dθ
 
     s_e = u*conj(i)# s=(v_d*i_d+v_q*i_q)+j(v_q*i_d-v_d*i_q)
     p_e = real(s_e)
     q_e = imag(s_e)
 
     # PI speed control:
-    e_p = -(ω_rref-ω_r)
-    de_Ip = e_p
-    t_e = K_r1*e_p+K_r2*e_IP
+    e_P = -(ω_rref-ω_r)
+    de_IP = e_P
+    t_e = K_r1*e_P+K_r2*e_IP
     p_in =t_e/ω_r
+    println("(ω_rref-ω_r): ",(ω_rref-ω_r))
+    println("e_IP: ",e_IP)
+    println("ω_r:",ω_r)
 
     t_m = P/ω_r
     dω_r = 1/J*(t_m-t_e)
+    println("p_e: ",p_e)
+    println("p_in: ",p_in)
 
     du_dc = 1/C*(p_in-p_e)
 
-    u_t=v_d#TODO: discuss!!!
+    u_t=abs(u)#TODO: discuss!!!
 
     # PI voltage control:
-    e_V = -(u_dcref-u_dc)
+    e_V = (u_dcref-u_dc)
     de_IV = e_V
     i_d = K_g1*e_V + K_g2*de_IV
+    println("u_tref: ",u_tref)
+    println("u_dc: ",u_dc)
 
     du_tref = K_Q*(Q_ref-q_e)
     di_q = K_v*(u_tref-u_t)
     println("v_d: ",v_d)
+    println("v_q: ",v_q)
+    println("abs(u)",abs(u))
 
     du = i - (i_d+1im*i_q)*exp(1im*θ)
 end
--- test/nodes/experimental/WindTurbineGenType4.jl
+++ test/nodes/experimental/WindTurbineGenType4.jl
@@ -5,20 +5,20 @@ using LinearAlgebra: I
 
 include("../NodeTestBase.jl")
 
-@testset "WindTurbine Tests" begin
+@testset "WindGenType 4 Tests" begin
     @syms K_PLL C J P ω_rref K_Q K_v K_g1 K_g2 K_r1 K_r2 positive=true
     @syms Q_ref P u_dcref real=true
     @syms θ dθ e_IP de_IP e_IV de_IV u_dc du_dc i_q di_q u_tref du_tref ω_r dω_r real=true
-    @syms f_INVALID k_PLL_INVALID negative=true
+    @syms C_INVALID k_PLL_INVALID negative=true
 
-    @test_throws AssertionError construct_vertex(WindTurbineGenType4(K_PLL=K_PLL,Q_ref=Q_ref,C=C,J=J,P=P,ω_rref=ω_rref,u_dcref=u_dcref,K_Q=K_Q,K_v=K_v,K_g1=K_g1,K_g2=K_g2,K_r1=K_r1,K_r2=K_r2))
-    @test_throws AssertionError construct_vertex(WindTurbineGenType4(K_PLL=K_PLL,Q_ref=Q_ref,C=C,J=J,P=P,ω_rref=ω_rref,u_dcref=u_dcref,K_Q=K_Q,K_v=K_v,K_g1=K_g1,K_g2=K_g2,K_r1=K_r1,K_r2=K_r2))
+    @test_throws AssertionError construct_vertex(WindTurbineGenType4(K_PLL=k_PLL_INVALID,Q_ref=Q_ref,C=C,J=J,P=P,ω_rref=ω_rref,u_dcref=u_dcref,K_Q=K_Q,K_v=K_v,K_g1=K_g1,K_g2=K_g2,K_r1=K_r1,K_r2=K_r2))
+    @test_throws AssertionError construct_vertex(WindTurbineGenType4(K_PLL=K_PLL,Q_ref=Q_ref,C=C_INVALID,J=J,P=P,ω_rref=ω_rref,u_dcref=u_dcref,K_Q=K_Q,K_v=K_v,K_g1=K_g1,K_g2=K_g2,K_r1=K_r1,K_r2=K_r2))
 
-    wind_gen = PVInverterWithFrequencyControl(K_PLL=K_PLL,Q_ref=Q_ref,C=C,J=J,P=P,ω_rref=ω_rref,u_dcref=u_dcref,K_Q=K_Q,K_v=K_v,K_g1=K_g1,K_g2=K_g2,K_r1=K_r1,K_r2=K_r2)
+    wind_gen = WindTurbineGenType4(K_PLL=K_PLL,Q_ref=Q_ref,C=C,J=J,P=P,ω_rref=ω_rref,u_dcref=u_dcref,K_Q=K_Q,K_v=K_v,K_g1=K_g1,K_g2=K_g2,K_r1=K_r1,K_r2=K_r2)
     wind_gen_vertex = construct_vertex(wind_gen)
 
-    @test symbolsof(wind_gen) == [:u_r,:u_i,:θ,:e_IP,:e_IV,:u_dc,:i_q,:u_tref,:ω]
+    @test symbolsof(wind_gen) == [:u_r,:u_i,:θ,:e_IP,:e_IV,:u_dc,:i_q,:u_tref,:ω_r]
     @test wind_gen_vertex.mass_matrix == [0,0,1,1,1,1,1,1,1]
 
-    smoketest_rhs(wind_gen_vertex, int_x=[omega,theta_PLL, v_xm, v_ym, P, ω], int_dx=[domega, dtheta_PLL, dvx_m, dv_ym, dP,dω])
+    smoketest_rhs(wind_gen_vertex, int_x=[θ,e_IP,e_IV,u_dc,i_q,u_tref, ω_r], int_dx=[dθ,de_IP,de_IV,du_dc,di_q,du_tref, dω_r])
 end
