69f2476a9c2a269f85ed9d81f905c854fb0c6def
Sabine Auer <sabine.auer@elena-international.com>
2019-10-29 18:59:13 +0100

    debuggin

--- src/nodes/experimental/WindTurbineGenType4.jl
+++ src/nodes/experimental/WindTurbineGenType4.jl
@@ -27,13 +27,13 @@ end [[θ_PLL,dθ_PLL],[e_IP,de_IP],[e_IV,de_IV],[u_dc,du_dc],[i_q,di_q],[u_tref,
     ω = ω_r-1-dθ_PLL
     Δt = -2*ω
 
-    function PI_control(e_I,e,K_P,K_I)
-        @assert K_P>=0
-        @assert K_I>=0
-        de_I=e
-        u = K_P*e+K_I*e_I
-        return [de_I,u]
-    end
+    #function PI_control(e_I,e,K_P,K_I)
+    #    @assert K_P>=0
+    #    @assert K_I>=0
+    #    de_I=e
+    #    u = K_P*e+K_I*e_I
+    #    return [de_I,u]
+    #end
 
     #u_dq = u*exp(-1im*θ)
     #dθ = v_d*K_PLL
@@ -47,7 +47,7 @@ end [[θ_PLL,dθ_PLL],[e_IP,de_IP],[e_IV,de_IV],[u_dc,du_dc],[i_q,di_q],[u_tref,
     # PI speed control:
     #de_IP,t_e=PI_control(e_IP,ω_rref-ω_r,K_r1,K_r2)
     #println("t_e:",t_e)
-    e_P = -(ω_rref-ω_r)
+    e_P = (ω_rref-ω_r)
     de_IP = e_P
     #println("e_IP: ",e_IP)
     t_e = K_r1*e_P+K_r2*e_IP
@@ -72,7 +72,7 @@ end [[θ_PLL,dθ_PLL],[e_IP,de_IP],[e_IV,de_IV],[u_dc,du_dc],[i_q,di_q],[u_tref,
 
     # PI voltage control:
     #de_IV,i_d=PI_control(e_IV,(u_dcref-u_dc),K_g1,K_g2)
-    e_V = -(u_dcref-u_dc)
+    e_V = (u_dcref-u_dc)
     de_IV = e_V
     i_d = K_g1*e_V + K_g2*e_IV
     println("id: ",i_d)
