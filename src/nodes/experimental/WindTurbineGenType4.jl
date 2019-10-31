94e5fac08cd47dbf276fee5f7297c758d37fc71c
Sabine Auer <sabine.auer@elena-international.com>
2019-10-31 14:27:17 +0100

    wtg model running without inertia

--- src/nodes/experimental/WindTurbineGenType4.jl
+++ src/nodes/experimental/WindTurbineGenType4.jl
@@ -7,7 +7,7 @@ WindTurbineGenType4(;I_n,k_PLL,f,f_s,T_m,k_P,τ_ω)
 """
 
 @DynamicNode WindTurbineGenType4(D,K_PLL,Q_ref,C,J,P,ω_rref,u_dcref,K_Q,K_v,K_g1,K_g2,K_r1,K_r2) begin
-    MassMatrix(m_u = false,m_int = [true,true,true,true,true])#,true,true])
+    MassMatrix(m_u = false,m_int = [true,true,true,true,true,true])#,true,true])
 end  begin
     @assert J>=0
     @assert K_g1>=0
@@ -17,7 +17,7 @@ end  begin
     @assert ω_rref>=0
     @assert C>=0
     @assert K_PLL>=0
-end [[θ_PLL,dθ_PLL],[e_IP,de_IP],[e_IV,de_IV],[u_dc,du_dc],[ω_r,dω_r]] begin#,[i_q,di_q],[u_tref,du_tref]] begin
+end [[θ_PLL,dθ_PLL],[e_Idθ,de_Idθ],[e_IP,de_IP],[e_IV,de_IV],[u_dc,du_dc],[ω_r,dω_r]] begin#,[i_q,di_q],[u_tref,du_tref]] begin
     function PI_control(e_I,e,K_P,K_I)
         @assert K_P>=0
         @assert K_I>=0
@@ -28,23 +28,29 @@ end [[θ_PLL,dθ_PLL],[e_IP,de_IP],[e_IV,de_IV],[u_dc,du_dc],[ω_r,dω_r]] begin
     u_dq = u*exp(-1im*θ_PLL)
     v_d = real(u_dq)
     v_q = imag(u_dq)
-    i_q = 0
+    i_q = 0.
 
-    #de_Idθ_PLL,dθ_LL=PI_control(e_Idθ_PLL,v_q,K_PLL,K_PLL)#v_q*K_PLL
 
-    dθ_PLL=v_q*K_PLL
+    de_Idθ,ω_PLL=PI_control(e_Idθ,-v_q,K_PLL,K_PLL)#v_q*K_PLL
+    println("ω_PLL: ",ω_PLL)
+
+    dθ_PLL=ω_PLL
     #println("dθ_PLL: ", dθ_PLL)
 
     #ω = ω_r-1-dθ_PLL
     #Δt = -2*ω
+    de_IV,i_d=PI_control(e_IV,(u_dcref-u_dc),K_g1,K_g2)
+    i_dq = (i_d+1im*i_q)
 
-    s_e = u*conj(i)# s=(v_d*i_d+v_q*i_q)+j(v_q*i_d-v_d*i_q)
+
+    s_e = u_dq*conj(i_dq)# s=(v_d*i_d+v_q*i_q)+j(v_q*i_d-v_d*i_q)
     p_e = real(s_e)
     q_e = imag(s_e)
 
     # speed control:
     de_IP,p_in=PI_control(e_IP,(ω_rref-ω_r),K_r1,K_r2)
-    t_e=p_in/ω_r
+    println("e_IP: ",e_IP)
+    t_e = p_in/ω_r
     t_m = P/ω_r
     println("t_e",t_e)
     dω_r = 1/J*(t_m-t_e)#-D*ω_r)
@@ -54,10 +60,11 @@ end [[θ_PLL,dθ_PLL],[e_IP,de_IP],[e_IV,de_IV],[u_dc,du_dc],[ω_r,dω_r]] begin
     println("p_e",p_e)
     println("q_e",q_e)
     du_dc = 1/C*(p_in-p_e)
-    de_IV,i_d=PI_control(e_IV,(u_dcref-u_dc),K_g1,K_g2)
+    println("e_IV: ",e_IV)
+
 
     # reactive power control
-    #u_t=v_q#TODO: discuss!!!
+    #u_t=abs(u)#TODO: discuss!!!
     #du_tref = K_Q*(Q_ref-q_e)
     #di_q = K_v*(u_tref-u_t)
 
@@ -66,11 +73,12 @@ end [[θ_PLL,dθ_PLL],[e_IP,de_IP],[e_IV,de_IV],[u_dc,du_dc],[ω_r,dω_r]] begin
     #println("u_tref: ",u_tref)
     #println("u_t: ",u_t)
     println("u_dc: ",u_dc)
-    println("u_dq",u_dq)
-    println("ω_r",ω_r)
+    println("u_dq: ",u_dq)
+    println("ω_r: ",ω_r)
+    println("i: ",i)
+    println("u: ",u)
     #println("ω",ω)
 
-    i_dq = (i_d+1im*i_q)
     #du = i_dq*u_dq-u*conj(i)
     du = i - i_dq*exp(1im*θ_PLL)
 end
