2798f38e19abe67aae8e2dee26af882cb002118b
Sabine Auer <sabine.auer@elena-international.com>
2019-11-09 22:25:00 +0100

    updates on node type

--- src/nodes/experimental/CurtailedPowerPlantWithInertia.jl
+++ src/nodes/experimental/CurtailedPowerPlantWithInertia.jl
@@ -3,46 +3,43 @@
 CurtailedPowerPlantWithInertia(;)
 ```
 """
-@DynamicNode CurtailedPowerPlantWithInertia(option1,P,K_ω,K_PPLL,K_IPLL,T_d,T_f) begin
-    MassMatrix(m_u = false,m_int = [true,true,true,true])
+@DynamicNode CurtailedPowerPlantWithInertia(P,ω_0,T_AI,K_PPLL,K_IPLL,T_d,T_f,K_PV,K_IV) begin
+    MassMatrix(m_u = false,m_int = [true,true,true,true,true,true])#true,true,true,true
 end  begin
     @assert K_PPLL>=0
-end [[θ_PLL,dθ_PLL],[e_Idθ,de_Idθ],[z,dz],[y,dy]] begin
-    function PI_control(e_I,e,K_P,K_I)
-        @assert K_P>=0
-        @assert K_I>=0
-        de_I=e
-        u = K_P*e+K_I*e_I
-        return [de_I,u]
-    end
+end [[θ_PLL,dθ_PLL],[e_Iω,de_Iω],[ω,dω],[y,dy],[e_Iiq,de_Iiq],[e_Iid,de_Iid]] begin#,,[i_d,di_d],[i_q,di_q],[v_q,dv_q],[v_d,dv_d]
 
     i_dq = i*exp(-1im*θ_PLL)
     u_dq = u*exp(-1im*θ_PLL)
     v_d = real(u_dq)
     v_q = imag(u_dq)
     i_d = real(i_dq)
+    i_q = imag(i_dq)
 
+    i_q_ref=0
     #PLL dynamics for frequency measurement
-    de_Idθ,ω_PLL=PI_control(e_Idθ,v_q,K_PPLL,K_IPLL)
+    e_ω= -v_q
+    de_Iω= e_ω
+    ω_PLL=K_PPLL*e_ω+K_IPLL*e_Iω
     dθ_PLL=ω_PLL
 
-    ω = 1/T_d*(ω_PLL*K_ω-z)
-    dz = ω
 
-    if option1
-        di_d_ref = 1/T_H*(i_d_ref-ω)
-        deId,v_d = PI_control(eId,i_d_ref-i_d,K_Pid,K_Iid)
-        deIq,u_q = PI_control(eIq,i_q_ref-i_q,K_Pid,K_Iid)
-        du = i - i_dq*exp(1im*θ_PLL)
-    else
-        p = real(u_dq * conj(i_dq))
-        P_I = 1/T_f*(ω-y)
-        dy = P_I
-        P_ref= P - P_I
-        println("i_d",i_d)
-        println("p",p)
-        du = p - P_ref
-    end
+    dω = 1/T_d*(ω_PLL-ω)
+
+    i_dI = 1/(T_f*ω_0)*(-ω-y*ω_0/T_AI)
+
+
+    dy = i_dI
+    i_d_ref= P/v_d+i_dI#(P-i_q*v_q)/v_d+i_dI
+    e_id = i_d_ref-i_d
+    de_Iid= e_id
+    v_d_ref=K_PV*e_id+K_IV*e_Iid
+
+    e_iq = i_q_ref-i_q
+    de_Iiq= e_iq
+    v_q_ref=K_PV*e_iq+K_IV*e_Iiq
+
+    du = u - (v_d_ref+1im*v_q_ref)*exp(1im*θ_PLL)
 end
 
 export CurtailedPowerPlantWithInertia
