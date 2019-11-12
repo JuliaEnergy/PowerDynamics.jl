05c4b4f1244b19a44a9b920aa1c4ca08887e9bb5
Sabine Auer <sabine.auer@elena-international.com>
2019-11-12 14:28:45 +0100

    cleaning up curtailed model

--- src/nodes/experimental/CurtailedPowerPlantWithInertia.jl
+++ src/nodes/experimental/CurtailedPowerPlantWithInertia.jl
@@ -3,19 +3,18 @@
 CurtailedPowerPlantWithInertia(;)
 ```
 """
-@DynamicNode CurtailedPowerPlantWithInertia(PICFunction,P,ω_0,T_AI,K_PPLL,K_IPLL,T_d,T_f,K_PV,K_IV) begin
-    MassMatrix(m_u = false,m_int = [true,true,true,true,true,true])#true,true,true,true
+@DynamicNode CurtailedPowerPlantWithInertia(P,ω_0,T_AI,K_PPLL,K_IPLL,T_d,T_f,K_PV,K_IV) begin
+    MassMatrix(m_u = false,m_int = [true,true,true,true,true,true])
 end  begin
     @assert K_PPLL>=0
-end [[θ_PLL,dθ_PLL],[e_Iω,de_Iω],[ω,dω],[y,dy],[e_Iiq,de_Iiq],[e_Iid,de_Iid]] begin#,,[i_d,di_d],[i_q,di_q],[v_q,dv_q],[v_d,dv_d]
-    function PI_control(e_I,e,K_P,K_I)
-        @assert K_P>=0
-        @assert K_I>=0
-        de_I=e
-        u = K_P*e+K_I*e_I
-        return [de_I,u]
-    end
-
+    @assert K_IPLL>=0
+    @assert K_PV>=0
+    @assert K_IV>=0
+    @assert T_d>=0
+    @assert T_f>=0
+    @assert T_AI>=0
+end [[θ_PLL,dθ_PLL],[e_Iω,de_Iω],[ω,dω],[y,dy],[e_Iiq,de_Iiq],[e_Iid,de_Iid]] begin
+    # transformation from global to local dq-coordinates
     i_dq = i*exp(-1im*θ_PLL)
     u_dq = u*exp(-1im*θ_PLL)
     v_d = real(u_dq)
@@ -30,29 +29,17 @@ end [[θ_PLL,dθ_PLL],[e_Iω,de_Iω],[ω,dω],[y,dy],[e_Iiq,de_Iiq],[e_Iid,de_Ii
     ω_PLL=K_PPLL*e_ω+K_IPLL*e_Iω
     dθ_PLL=ω_PLL
 
-
-    dω = 1/T_d*(ω_PLL-ω)
-
-    i_dI = 1/(T_f*ω_0)*(-ω-y*ω_0)
+    # inertia emulation with df/dt response
+    dω = 1/T_d*(ω_PLL-ω) # low pass filter
+    i_dI = 1/(T_f*ω_0)*(-ω-y*ω_0) # high pass filter
     dy = i_dI
-    i_d_ref= P/v_d+T_AI*i_dI#(P-i_q*v_q)/v_d+i_dI
+    i_d_ref= P/v_d+T_AI*i_dI
 
     # current controller
-    if PICFunction
-        de_Iid,v_d_ref = PIControl(e_Iid,i_d_ref-i_d,K_PV,K_IV)
-        de_Iiq,v_q_ref = PIControl(e_Iiq,i_q_ref-i_q,K_PV,K_IV)
-    else
-        e_id = i_d_ref-i_d
-        de_Iid= e_id
-        v_d_ref=K_PV*e_id+K_IV*e_Iid
-
-        e_iq = i_q_ref-i_q
-        de_Iiq= e_iq
-        v_q_ref=K_PV*e_iq+K_IV*e_Iiq
-    end
+    de_Iid,v_d_ref = PIControl(e_Iid,i_d_ref-i_d,K_PV,K_IV)
+    de_Iiq,v_q_ref = PIControl(e_Iiq,i_q_ref-i_q,K_PV,K_IV)
 
     du = u - (v_d_ref+1im*v_q_ref)*exp(1im*θ_PLL)
-    #0 = i - (i_d_ref+1im*i_q_ref)*exp(1im*θ_PLL)
 end
 
 export CurtailedPowerPlantWithInertia
