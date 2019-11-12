9a4e95396a6fc03c147701c74df47b4d0c765938
SabineAuer <sabine.auer@elena-international.com>
2019-11-12 14:23:03 +0100

    Integrate wind turbine model with inertia emulation (#65)

--- test/nodes/experimental/WindTurbineGenType4_RotorControl.jl
+++ test/nodes/experimental/WindTurbineGenType4_RotorControl.jl
@@ -4,19 +4,18 @@ using PowerDynamics: WindTurbineGenType4_RotorControl, construct_vertex, symbols
 using LinearAlgebra: I
 
 include("../NodeTestBase.jl")
-
+#T_L,K_dbr,T_H,K_ω,K_PLL,Q_ref,C,J,P,ω_rref,u_dcref,K_Q,K_v,K_pv,K_iv,K_ptrq,K_itrq
 @testset "WindGenType 4 with Rotor Control Tests" begin
-    #T_L,T_H,K_P,K_PLL,Q_ref,C,J,P,ω_rref,u_dcref,K_Q,K_v,K_g1,K_g2,K_r1,K_r2
-    #[θ_PLL,dθ_PLL],[e_Idθ,de_Idθ],[ω,dω],[e_IP,de_IP],[z,dz],[e_IV,de_IV],[u_dc,du_dc],[i_q,di_q],[u_tref,du_tref],[ω_r,dω_r]
-    @syms T_L T_H K_P K_PLL C J u_dcref K_Q K_v K_g1 K_g2 K_r1 K_r2 positive=true
+    @syms T_L ω_0 T_H K_ω K_PPLL K_IPLL C J u_dcref K_Q K_v K_pv K_iv K_ptrq K_itrq positive=true
     @syms Q_ref ω_rref P real=true
     @syms θ_PLL dθ_PLL e_Idθ de_Idθ ω dω  e_IP de_IP z dz e_IV de_IV u_dc du_dc i_q di_q u_tref du_tref ω_r dω_r real=true
     @syms k_P_invalid k_PLL_INVALID negative=true
 
-    @test_throws AssertionError construct_vertex(WindTurbineGenType4_RotorControl(T_L=T_L,T_H=T_H,K_P=K_P,K_PLL=k_PLL_INVALID,Q_ref=Q_ref,C=C,J=J,P=P,ω_rref=ω_rref,u_dcref=u_dcref,K_Q=K_Q,K_v=K_v,K_g1=K_g1,K_g2=K_g2,K_r1=K_r1,K_r2=K_r2))
-    @test_throws AssertionError construct_vertex(WindTurbineGenType4_RotorControl(T_L=T_L,T_H=T_H,K_P=k_P_invalid,K_PLL=K_PLL,Q_ref=Q_ref,C=C,J=J,P=P,ω_rref=ω_rref,u_dcref=u_dcref,K_Q=K_Q,K_v=K_v,K_g1=K_g1,K_g2=K_g2,K_r1=K_r1,K_r2=K_r2))
+    @test_throws AssertionError construct_vertex(WindTurbineGenType4_RotorControl(T_L=T_L,ω_0=ω_0,T_H=T_H,K_ω=K_ω,K_PPLL=k_PLL_INVALID,K_IPLL=K_IPLL,Q_ref=Q_ref,C=C,J=J,P=P,ω_rref=ω_rref,u_dcref=u_dcref,K_Q=K_Q,K_v=K_v,K_pv=K_pv,K_iv=K_iv,K_ptrq=K_ptrq,K_itrq=K_itrq))
+    @test_throws AssertionError construct_vertex(WindTurbineGenType4_RotorControl(T_L=T_L,ω_0=ω_0,T_H=T_H,K_ω=k_P_invalid,K_PPLL=K_PPLL,K_IPLL=K_IPLL,Q_ref=Q_ref,C=C,J=J,P=P,ω_rref=ω_rref,u_dcref=u_dcref,K_Q=K_Q,K_v=K_v,K_pv=K_pv,K_iv=K_iv,K_ptrq=K_ptrq,K_itrq=K_itrq))
 
-    wind_gen = WindTurbineGenType4_RotorControl(T_L=T_L,T_H=T_H,K_P=K_P,K_PLL=K_PLL,Q_ref=Q_ref,C=C,J=J,P=P,ω_rref=ω_rref,u_dcref=u_dcref,K_Q=K_Q,K_v=K_v,K_g1=K_g1,K_g2=K_g2,K_r1=K_r1,K_r2=K_r2)
+                                               #T_L,    K_dbr,      T_H,    K_ω,    K_PPLL,     K_IPLL,        Q_ref,      C,  J,  P,  ω_rref,       u_dcref,        K_Q,    K_v,    K_pv,     K_iv,     K_ptrq,       K_itrq
+    wind_gen = WindTurbineGenType4_RotorControl(T_L=T_L,ω_0=ω_0,T_H=T_H,K_ω=K_ω,K_PPLL=K_PPLL,K_IPLL=K_IPLL, Q_ref=Q_ref,C=C,J=J,P=P,ω_rref=ω_rref,u_dcref=u_dcref,K_Q=K_Q,K_v=K_v,K_pv=K_pv,K_iv=K_iv,K_ptrq=K_ptrq,K_itrq=K_itrq)
     wind_gen_vertex = construct_vertex(wind_gen)
 
     @test symbolsof(wind_gen) == [:u_r,:u_i,:θ_PLL,:e_Idθ,:ω,:e_IP,:z,:e_IV,:u_dc,:i_q,:u_tref,:ω_r]
