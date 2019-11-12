774b6dd9a07a554a05c51c9f7f761fb66e6be06c
Sabine Auer <sabine.auer@elena-international.com>
2019-11-12 13:48:36 +0100

    parameters corrected for wtg model

--- src/nodes/experimental/WindTurbineGenType4_RotorControl.jl
+++ src/nodes/experimental/WindTurbineGenType4_RotorControl.jl
@@ -1,12 +1,12 @@
 @doc doc"""
 ```Julia
-WindTurbineGenType4(;I_n,k_PLL,f,f_s,T_m,k_P,τ_ω)
+WindTurbineGenType4_RotorControl(;T_L,ω_0,T_H,K_ω,K_PPLL,K_IPLL,Q_ref,C,J,P,ω_rref,u_dcref,K_Q,K_v,K_pv,K_iv,K_ptrq,K_itrq) begin
 ```
 
 ```
 """
 
-@DynamicNode WindTurbineGenType4_RotorControl(T_L,K_dbr,T_H,K_ω,K_PPLL,K_IPLL,Q_ref,C,J,P,ω_rref,u_dcref,K_Q,K_v,K_pv,K_iv,K_ptrq,K_itrq) begin
+@DynamicNode WindTurbineGenType4_RotorControl(T_L,ω_0,T_H,K_ω,K_PPLL,K_IPLL,Q_ref,C,J,P,ω_rref,u_dcref,K_Q,K_v,K_pv,K_iv,K_ptrq,K_itrq) begin
     MassMatrix(m_u = false,m_int = [true,true,true,true,true,true,true,true,true,true])
 end  begin
     @assert J>=0
@@ -19,33 +19,23 @@ end  begin
     @assert K_IPLL>=0
     @assert K_ω>=0
 end [[θ_PLL,dθ_PLL],[e_Idθ,de_Idθ],[ω,dω],[e_IP,de_IP],[z,dz],[e_IV,de_IV],[u_dc,du_dc],[i_q,di_q],[u_tref,du_tref],[ω_r,dω_r]] begin
-    function PI_control(e_I,e,K_P,K_I)
-      @assert K_P>=0
-      @assert K_I>=0
-      de_I=e
-      u = K_P*e+K_I*e_I
-      return [de_I,u]
-    end
     # transformation from global to local dq-coordinates with θ_PLL
     u_dq = u*exp(-1im*θ_PLL)
     v_d = real(u_dq)
     v_q = imag(u_dq)
 
     # PLL dynamics for frequency measurement
-    de_Idθ,ω_PLL=PI_control(e_Idθ,-v_q,K_PPLL,K_IPLL)
+    de_Idθ,ω_PLL=PIControl(e_Idθ,-v_q,K_PPLL,K_IPLL)
     dθ_PLL=ω_PLL
 
-    #println(ω_PLL)
-
     # PI speed control:
-    ##k_pp=150, kip=25
-    de_IP,t_eref=PI_control(e_IP,ω_rref-ω_PLL-ω_r,K_ptrq,K_itrq)
+    de_IP,t_eref=PIControl(e_IP,ω_rref-ω_PLL-ω_r,K_ptrq,K_itrq)
 
     # inertial respons emulation
-    dω=1/T_L*(ω_PLL*K_dbr-ω)
-    Δt_eref=-1/T_H*(-z+ω*K_ω*T_H)
+    dω=1/T_L*(ω_PLL-ω)
+    Δt_eref=1/(T_H*ω_0)*(-z*ω_0-ω)
     dz = Δt_eref
-    t_e =(t_eref+Δt_eref)
+    t_e =(t_eref+Δt_eref*K_ω)
 
     # turbine dynamics
     t_m = P/ω_r
@@ -53,7 +43,7 @@ end [[θ_PLL,dθ_PLL],[e_Idθ,de_Idθ],[ω,dω],[e_IP,de_IP],[z,dz],[e_IV,de_IV]
     dω_r = 1/J*(t_m-t_e)#-D*ω_r)
 
     # DC voltage control
-    de_IV,i_d=PI_control(e_IV,(u_dcref-u_dc),K_pv,K_iv)
+    de_IV,i_d=PIControl(e_IV,(u_dcref-u_dc),K_pv,K_iv)
     i_dq = i_d+1im*i_q
     s_e = u_dq*conj(i_dq)# s=(v_d*i_d+v_q*i_q)+j(v_q*i_d-v_d*i_q)
     p_e = real(s_e)
--- test_WTG.jl
+++ test_WTG.jl
@@ -5,19 +5,20 @@ using Revise
 
 
 node_list=[]
-    #append!(node_list,[SlackAlgebraic(U=1.)])
-    append!(node_list, [SwingEqLVS(H=2, P=-1., D=0.1, Ω=50,Γ=0.1,V=1.)])
+    append!(node_list,[SlackAlgebraic(U=1.)])
+    append!(node_list, [SwingEqLVS(H=5, P=-1., D=0.1, Ω=2π*50,Γ=0.1,V=1.)])
     #append!(node_list,[PQAlgebraic(P=-0.5,Q=0)])
     #append!(node_list,[FESS(u_dc_ref=1,D=1,C=1,J=0.018,ω_m_ref=0,k_PLL=1,f=50,L_g=0.001,L_m=0.01,P_n=2,Ψ_m=0.12,R_m=1,R_g=0.1,K_m1=5,K_m2=50,K_m3=0.1,K_m4=1,K_g1=5,K_g2=1000,K_g3=0.5,K_g4=1)])
     #append!(node_list,[PVInverterWithFrequencyControl(I_n=1.,k_PLL=20.,f=50,f_s=50.05,T_m=0.1,k_P=0.2,τ_ω=1.)])
-    append!(node_list, [WindTurbineGenType4_RotorControl(T_L=1,K_dbr=1,T_H=5.5,K_ω=10,K_PPLL=1000,K_IPLL=10070,Q_ref=0,C=1,J=4,P=1,ω_rref=1,u_dcref=1,K_Q=0.1,K_v=40,K_pv=10,K_iv=10,K_ptrq=3,K_itrq=0.6)])
+    append!(node_list, [WindTurbineGenType4_RotorControl(T_L=0.0001,ω_0=2π*50,T_H=0.0055,K_ω=10,K_PPLL=50,K_IPLL=625,Q_ref=0,C=1,J=4,P=1,ω_rref=1,u_dcref=1,K_Q=0.1,K_v=400,K_pv=10,K_iv=10,K_ptrq=3*10,K_itrq=0.6*10)])
     #append!(node_list,[WindTurbineGenType4_RotorControl(T_L=1,K_dbr=0.0025,T_H=5.5,K_ω=10,K_PPLL=18.4,K_IPLL=170,Q_ref=0,C=1,J=4,P=1,ω_rref=1,u_dcref=1,K_Q=0.1,K_v=40,K_pv=10,K_iv=10,K_ptrq=3,K_itrq=0.6)])
 
     #append!(node_list, [WindTurbineGenType4_RotorControl(T_L=1,T_H=0.5,K_P=0.2,K_PLL=1,Q_ref=0,C=1,J=1,P=1,ω_rref=1,u_dcref=1,K_Q=1,K_v=1,K_pv=1,K_iv=100,K_pp=1,K_ip=10)])
     #append!(node_list, [WindTurbineGenType4(ΔP_max=0.3,k_P=0.2,K_PLL=10,Q_ref=0,C=1,J=1,P=1,ω_rref=1,K_Q=1,K_v=1,K_g1=1,K_g2=10,K_r1=1,K_r2=1)])
     line_list=[]
-    append!(line_list,[StaticLine(from=1,to=2,Y=-1im/0.02)])
-    #append!(line_list,[StaticLine(from=2,to=3,Y=-1im/0.02)])
+    append!(line_list,[PiModelLine(from=1, to=2, y=-1im/0.02, y_shunt_km=0.02*1im, y_shunt_mk=0.02*1im)])
+    #append!(line_list,[StaticLine(from=1,to=2,Y=-1im/0.02)])
+    append!(line_list,[PiModelLine(from=2, to=3, y=-1im/0.02, y_shunt_km=0.02*1im, y_shunt_mk=0.02*1im)])
 
 powergrid = PowerGrid(node_list,line_list)
 
@@ -31,7 +32,7 @@ powergrid = PowerGrid(node_list,line_list)
 operationpoint = find_operationpoint(powergrid)
 
 
-disturbed_node=1;
+disturbed_node=2;
 final_time=2
 simulation_time = (0.0,final_time);
 pd = PowerPerturbation(node_number=disturbed_node,fraction=0.9, tspan_fault=(0.5,2));
