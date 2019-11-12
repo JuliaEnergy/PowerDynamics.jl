74a3dee8d2a370b3b3e6058febf95cde155a7282
Sabine Auer <sabine.auer@elena-international.com>
2019-11-12 08:43:22 +0100

    testing

--- src/nodes/experimental/CurtailedPowerPlantWithInertia.jl
+++ src/nodes/experimental/CurtailedPowerPlantWithInertia.jl
@@ -8,6 +8,13 @@ CurtailedPowerPlantWithInertia(;)
 end  begin
     @assert K_PPLL>=0
 end [[θ_PLL,dθ_PLL],[e_Iω,de_Iω],[ω,dω],[y,dy],[e_Iiq,de_Iiq],[e_Iid,de_Iid]] begin#,,[i_d,di_d],[i_q,di_q],[v_q,dv_q],[v_d,dv_d]
+    function PI_control(e_I,e,K_P,K_I)
+        @assert K_P>=0
+        @assert K_I>=0
+        de_I=e
+        u = K_P*e+K_I*e_I
+        return [de_I,u]
+    end
 
     i_dq = i*exp(-1im*θ_PLL)
     u_dq = u*exp(-1im*θ_PLL)
@@ -26,10 +33,11 @@ end [[θ_PLL,dθ_PLL],[e_Iω,de_Iω],[ω,dω],[y,dy],[e_Iiq,de_Iiq],[e_Iid,de_Ii
 
     dω = 1/T_d*(ω_PLL-ω)
 
-    i_dI = 1/(T_f*ω_0)*(-ω-y*ω_0/T_AI)
+    i_dI = 1/(T_f*ω_0)*(-ω-y*ω_0)
     dy = i_dI
-    i_d_ref= P/v_d+i_dI#(P-i_q*v_q)/v_d+i_dI
+    i_d_ref= P/v_d+T_AI*i_dI#(P-i_q*v_q)/v_d+i_dI
 
+    # current controller
     if PICFunction
         de_Iid,v_d_ref = PIControl(e_Iid,i_d_ref-i_d,K_PV,K_IV)
         de_Iiq,v_q_ref = PIControl(e_Iiq,i_q_ref-i_q,K_PV,K_IV)
@@ -44,6 +52,7 @@ end [[θ_PLL,dθ_PLL],[e_Iω,de_Iω],[ω,dω],[y,dy],[e_Iiq,de_Iiq],[e_Iid,de_Ii
     end
 
     du = u - (v_d_ref+1im*v_q_ref)*exp(1im*θ_PLL)
+    #0 = i - (i_d_ref+1im*i_q_ref)*exp(1im*θ_PLL)
 end
 
 export CurtailedPowerPlantWithInertia
