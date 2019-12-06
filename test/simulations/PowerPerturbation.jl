e2a1c1f8e09e0862ea12fae794776b535bb3f382
Jan Liße <jan.lisse@posteo.de>
2019-12-06 17:19:51 +0100

    Quickfix for type instability in PowerPerturbation (#72)
    Travis CI is broken but tests run through locally.

--- test/simulations/PowerPerturbation.jl
+++ test/simulations/PowerPerturbation.jl
@@ -2,7 +2,7 @@ using Test: @test,@testset
 using PowerDynamics: PowerPerturbation, simulate, SwingEqLVS, StaticLine, PowerGrid, State,systemsize
 
 @testset "test PowerPerturbation simulation" begin
-    nodes = [SwingEqLVS(H=1., P=-1, D=1, Ω=50, Γ=20, V=1), SwingEqLVS(H=1., P=1, D=1, Ω=50, Γ=20, V=1)]
+    nodes = [SwingEqLVS(H=1., P=-1.0, D=1, Ω=50, Γ=20, V=1), SwingEqLVS(H=1., P=1.0, D=1, Ω=50, Γ=20, V=1)]
     lines = [StaticLine(Y=-50*im, from=1, to=2)]
     grid = PowerGrid(nodes, lines)
     state = State(grid, rand(systemsize(grid)))
