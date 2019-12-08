a68e158dc04df5d868bb4c706e413a4acc42e96e
Jan Lisse <jan.lisse@posteo.de>
2019-12-08 01:13:42 +0100

    Better solution for PowerPerturbation type instability

--- src/simulations/PowerPerturbation.jl
+++ src/simulations/PowerPerturbation.jl
@@ -14,16 +14,34 @@ struct PowerPerturbation
     node_number
     fraction
     tspan_fault
+    power_symbol
 end
-PowerPerturbation(;node_number,fraction,tspan_fault) = PowerPerturbation(node_number,fraction,tspan_fault)
+
+"Error to be thrown if something goes wrong during power perturbation"
+struct PowerPerturbationError <: PowerDynamicsError
+    msg::String
+end
+
+PowerPerturbation(;node_number,fraction,tspan_fault,power_symbol) = PowerPerturbation(node_number,fraction,tspan_fault,power_symbol)
 
 function (pd::PowerPerturbation)(powergrid)
-    #TODO: check if node type supported for power drop
     node_list_power_drop = copy(powergrid.nodes)
     node_for_drop = node_list_power_drop[pd.node_number]
-    node_for_drop = @set node_for_drop.P = round(Int64, node_for_drop.P*pd.fraction)
-    node_list_power_drop[pd.node_number] = node_for_drop
-    PowerGrid(node_list_power_drop, powergrid.lines)
+    if !(pd.power_symbol in fieldnames(typeof(node_for_drop)))
+        throw(PowerPerturbationError("Node number: $(pd.node_number) must have a power parameter: $(pd.power_symbol)"))
+    end
+    try
+        new_P = typeof(node_for_drop.P)(node_for_drop.P * pd.fraction)
+        node_for_drop = @set node_for_drop.P = new_P
+        node_list_power_drop[pd.node_number] = node_for_drop
+        PowerGrid(node_list_power_drop, powergrid.lines)
+    catch e
+        if isa(e, InexactError)
+            throw(PowerPerturbationError("Node number: $(pd.node_number) must have a $(pd.power_symbol) parameter of type Float64 for PowerPerturbation, but was of type $(typeof(node_for_drop.P))"))
+        else
+            throw(x) #handle every other error
+        end
+    end
 end
 
 """
@@ -60,4 +78,5 @@ function simulate(pd::PowerPerturbation, powergrid, x0, timespan)
 end
 
 export PowerPerturbation
+export PowerPerturbationError
 export simulate
--- test/simulations/PowerPerturbation.jl
+++ test/simulations/PowerPerturbation.jl
@@ -1,5 +1,5 @@
-using Test: @test,@testset
-using PowerDynamics: PowerPerturbation, simulate, SwingEqLVS, StaticLine, PowerGrid, State,systemsize
+using Test: @test,@testset, @test_throws
+using PowerDynamics: PowerPerturbation, simulate, SwingEqLVS, SlackAlgebraic, StaticLine, PowerGrid, State,systemsize, PowerPerturbationError
 
 @testset "test PowerPerturbation simulation" begin
     nodes = [SwingEqLVS(H=1., P=-1.0, D=1, Ω=50, Γ=20, V=1), SwingEqLVS(H=1., P=1.0, D=1, Ω=50, Γ=20, V=1)]
@@ -10,8 +10,38 @@ using PowerDynamics: PowerPerturbation, simulate, SwingEqLVS, StaticLine, PowerG
     sol = simulate(PowerPerturbation(
         fraction = 0.9,
         node_number = 1,
-        tspan_fault = (0.1,1)),
+        tspan_fault = (0.1,1),
+        power_symbol = :P),
         grid, state, (0., 1.))
     @test sol != nothing
     @test sol.dqsol.retcode == :Success
+end
+
+@testset "test PowerPerturbation simulation invalid P should throw PowerPerturbationError" begin
+    invalidP = 1
+    nodes = [SwingEqLVS(H=1., P=invalidP, D=1, Ω=50, Γ=20, V=1), SwingEqLVS(H=1., P=invalidP, D=1, Ω=50, Γ=20, V=1)]
+    lines = [StaticLine(Y=-50*im, from=1, to=2)]
+    grid = PowerGrid(nodes, lines)
+    state = State(grid, rand(systemsize(grid)))
+
+    @test_throws PowerPerturbationError simulate(PowerPerturbation(
+        fraction = 0.9,
+        node_number = 1,
+        tspan_fault = (0.1,1),
+        power_symbol = :P),
+        grid, state, (0., 1.))
+end
+
+@testset "test PowerPerturbation simulation node without P should throw PowerPerturbationError" begin
+    nodes = [SlackAlgebraic(U=-1*im), SwingEqLVS(H=1., P=-1.0, D=1, Ω=50, Γ=20, V=1)]
+    lines = [StaticLine(Y=-50*im, from=1, to=2)]
+    grid = PowerGrid(nodes, lines)
+    state = State(grid, rand(systemsize(grid)))
+
+    @test_throws PowerPerturbationError simulate(PowerPerturbation(
+        fraction = 0.9,
+        node_number = 1,
+        tspan_fault = (0.1,1),
+        power_symbol = :P),
+        grid, state, (0., 1.))
 end
