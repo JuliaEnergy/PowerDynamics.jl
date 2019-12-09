14fc2c9ded988c34c99c2690544467c3689af75b
Paul Schultz <pschultz@pik-potsdam.de>
2019-12-09 17:35:45 +0100

    composite types

--- src/nodes/CompositeNode.jl
+++ src/nodes/CompositeNode.jl
@@ -1,4 +1,3 @@
-using SparseArrays
 
 @doc doc"""
 ```julia
@@ -18,71 +17,101 @@ In principle we could build a composite node that incorporates power sources,
 current sources, and one voltage regulator.
 """
 
-@__doc__ struct CompositeNode{T}
-    CurrentNodes::T
-    idims::Array{Int, 1}
+struct _IPU end
+struct _IU end
+struct _PU end
+struct _IP end
+struct _I end
+struct _P end
+
+
+CompositionTypes = Dict(["IPU" => _IPU,
+    "IU" => _IU,
+    "PU" => _PU,
+    "I" => _I,
+    "P" => _P,
+    "IP" => _IP])
+
+@__doc__ mutable struct CompositeNode{T}
+    CurrentNodes # constant current nodes
+    VoltageNode # constant voltage nodes
+    PowerNodes # constant power nodes
+    internaldims::Array{Int, 1}
     total_dim::Int
     idxs::Array{Array{Int,1},1}
     symbols::Array{Symbol, 1}
-    function CompositeNode(CurrentNodes)# PowerNodes, VoltageNode)
-        symbols = [:u_r, :u_i]
-        idims = Array{Int,1}()
+    function CompositeNode(;CurrentNodes=nothing, VoltageNode=nothing, PowerNodes=nothing)# PowerNodes, VoltageNode)
 
-        for current_node in CurrentNodes
-            append!(symbols, symbolsof(current_node)[3:end])
-            append!(idims, dimension(current_node) - 2)
+        @assert any([CurrentNodes != nothing, PowerNodes != nothing])
+
+        @assert !(typeof(VoltageNode) <: AbstractArray)
+
+        symbols = [:u_r, :u_i]
+        internaldims = Array{Int,1}()
+        mix = ""
+
+        if CurrentNodes != nothing
+            mix *= "I"
+            for current_node in CurrentNodes
+                append!(symbols, symbolsof(current_node)[3:end])
+                append!(internaldims, dimension(current_node) - 2)
+            end
         end
 
-        # for power_node in PowerNodes
-        #     append!(symbols, symbolsof(power_node)[3:end])
-        #     append!(idims, dimension(power_node) - 2)
-        # end
+
+        if PowerNodes != nothing
+            mix *= "P"
+            for power_node in PowerNodes
+                append!(symbols, symbolsof(power_node)[3:end])
+                append!(internaldims, dimension(power_node) - 2)
+            end
+        end
+
+
+        if VoltageNode != nothing
+            mix *= "U"
+            append!(symbols, symbolsof(VoltageNode)[3:end])
+            append!(internaldims, dimension(VoltageNode) - 2)
+        end
 
         idxs = Array{Array{Int,1},1}([])
         offset = 2
-        for idim in idims
-            append!(idxs, [[[1,2]; (offset+1):(offset+idim)]])
+        for idim in internaldims
+            println([[[1,2]; (offset + 1):(offset + idim)]])
+            append!(idxs, [[[1,2]; (offset + 1):(offset + idim)]])
             offset += idim
         end
 
-        # p_idxs = Array{Array{Int,1},1}([])
-        # for idim in p_idims
-        #     append!(p_idxs, [[[1,2]; (offset+1):(offset+idim)]])
-        #     offset += idim
-        # end
+        total_dim = 2 + sum(internaldims)
+        CT = CompositionTypes[mix]
 
-
-        total_dim = 2 + sum(idims)
-
-        new{typeof(CurrentNodes)}(CurrentNodes,
-        idims,
+        new{CT}(CurrentNodes,
+        VoltageNode,
+        PowerNodes,
+        internaldims,
         total_dim,
         idxs,
         symbols)
     end
 end
 
-function construct_vertex(cn::CompositeNode{T}) where T
 
-    CN_Type = typeof(cn)
+function construct_vertex(cn::CompositeNode{T}) where T <: Union{_IPU, _IU, _PU}
 
-    current_nodes = [construct_vertex(cnode) for cnode in cn.CurrentNodes]
+    # first collect the information
+    current_vertices = [construct_vertex(n) for n in cn.CurrentNodes]
+    power_vertices = [construct_vertex(n) for n in cn.PowerNodes]
+    voltage_vertex = construct_vertex(cn.VoltageNode)
 
-    cfs = [cn.f! for cn in current_nodes]
+    all_vertices = vcat(current_nodes, power_nodes, voltage_vertex)
 
-    mass_matrices = [cn.mass_matrix for cn in current_nodes]
+    cfs = [n.f! for n in current_vertices]
+    pfs = [n.f! for n in power_vertices]
 
-    # Mass matrix is assumed to be a different type currently? Double check this
-    mass_matrix = spzeros(cn.total_dim, cn.total_dim)
+    mass_matrix = vcat(voltage_vertex.mass_matrix[1:2], [cn.mass_matrix[3:end] for cn in all_vertices]...)
 
-    for (i, mm) in enumerate(mass_matrices)
-        mass_matrix[cn.idxs[i], cn.idxs[i]] .= mm
-    end
-
-    mass_matrix[1, :] .= 0.
-    mass_matrix[2, :] .= 0.
-    mass_matrix[:, 1] .= 0.
-    mass_matrix[:, 2] .= 0.
+    num_cv = length(current_vertices)
+    num_pv = length(power_vertices)
 
     function rhs!(dx, x, e_s, e_d, p, t)
         i = total_current(e_s, e_d)
@@ -90,38 +119,89 @@ function construct_vertex(cn::CompositeNode{T}) where T
         # p = u*conj(i)
 
         i_injected = 0.0 + 0.0im
+        p_injected = 0.0 + 0.0im
 
         @views begin
 
-        for (cf, idx) in zip(cfs, cn.idxs)
-            cf(dx[idx], x[idx], e_s, e_d, p, t)
-            # The current nodes are assumed to have du = i - I_node
-            i_injected += i - complex(dx[1], dx[2])
-        end
+            # constant current
+            for (cf, idx) in zip(cfs, cn.idxs[1:num_cv])
+                cf(dx[idx], x[idx], e_s, e_d, p, t)
+                # The current nodes are assumed to have du = i - I_node
+                i_injected += i - complex(dx[1], dx[2])
+            end
 
-        # p_injected = 0.0 + 0.0im
+            for (pf, idx) in zip(pfs, cn.idxs[num_cv+1:num_cv+num_pv])
+                pf(dx[idx], x[idx], e_s, e_d, p, t)
+                # The current nodes are assumed to have du = p - P_node
+                p_injected += p - complex(dx[1], dx[2])
+            end
 
-        # for (pf, idx) in zip(pfs, cn.idxs)
-        #     pf(dx[idx], x[idx], e_s, e_d, p, t)
-        #     # The current nodes are assumed to have du = p - P_node
-        #     p_injected += p - complex(dx[1], dx[2])
-        # end
+            total_i = i_injected + p_injected / u
 
-        end
+            voltage_vertex.f!(dx[cn.idxs[end]], x[cn.idxs[end]], [e_s;[real(total_i), imag(total_i), 0., 0.]], e_d, p, t)
 
-        du = i - i_injected # - p_injected/u
-        dx[1] = real(du)
-        dx[2] = imag(du)
+        end # views
 
-        # Or if we had a voltage node:
-        # voltage(dx[idx_v], x[idx_v], [e_s;[real(i_injected), imag(i_injected), 0., 0.]], e_d, p, t)
     end
 
     ODEVertex(f! = rhs!, dim=cn.total_dim, mass_matrix=mass_matrix, sym=cn.symbols)
 end
 
-symbolsof(cn) = cn.symbols
 
-dimension(cn) = cn.total_dim
+
+function construct_vertex(cn::CompositeNode{T}) where T <: Union{_IP, _I, _P}
+
+    # first collect the information
+    current_vertices = [construct_vertex(n) for n in cn.CurrentNodes]
+    power_vertices = [construct_vertex(n) for n in cn.PowerNodes]
+
+    all_vertices = vcat(current_nodes, power_nodes)
+
+    cfs = [n.f! for n in current_vertices]
+    pfs = [n.f! for n in power_vertices]
+
+    mass_matrix = vcat([0,0], [cn.mass_matrix[3:end] for cn in all_vertices]...)
+
+    num_cv = length(current_vertices)
+    num_pv = length(power_vertices)
+
+    function rhs!(dx, x, e_s, e_d, p, t)
+        i = total_current(e_s, e_d)
+        u = x[1] + x[2] * im
+        # p = u*conj(i)
+
+        i_injected = 0.0 + 0.0im
+        p_injected = 0.0 + 0.0im
+
+        @views begin
+
+            # constant current
+            for (cf, idx) in zip(cfs, cn.idxs[1:num_cv])
+                cf(dx[idx], x[idx], e_s, e_d, p, t)
+                # The current nodes are assumed to have du = i - I_node
+                i_injected += i - complex(dx[1], dx[2])
+            end
+
+            for (pf, idx) in zip(pfs, cn.idxs[num_cv+1:num_cv+num_pv])
+                pf(dx[idx], x[idx], e_s, e_d, p, t)
+                # The current nodes are assumed to have du = p - P_node
+                p_injected += p - complex(dx[1], dx[2])
+            end
+
+            total_p = u * conj(i_injected - i) + p_injected # Check signs
+
+            dx[1] = real(total_p)
+            dx[2] = imag(total_p)
+
+        end # views
+
+    end
+
+    ODEVertex(f! = rhs!, dim=cn.total_dim, mass_matrix=mass_matrix, sym=cn.symbols)
+end
+
+symbolsof(cn::CompositeNode) = cn.symbols
+
+dimension(cn::CompositeNode) = cn.total_dim
 
 export CompositeNode
--- test/nodes/CompositeNode.jl
+++ test/nodes/CompositeNode.jl
@@ -1,9 +1,40 @@
+using Revise
+
 using Test: @testset, @test
-using PowerDynamics: CompositeNode, CSIMinimal, construct_vertex, symbolsof
+using PowerDynamics#: CompositeNode, CSIMinimal, VSIMinimal, PQAlgebraic, construct_vertex, symbolsof
 using SparseArrays
 
+# constant voltage
+VSI = VSIMinimal(τ_P=1.,τ_Q=1.,K_P=1.,K_Q=1.,V_r=1.,P=1.,Q=1.)
+
+# constant current
+CSI = CSIMinimal(I_r=1.)
+CSI2 = CSIMinimal(I_r=1im)
+
+# constant power
+PQ = PQAlgebraic(P=0.1,Q=0.1)
+PQ2 = PQAlgebraic(P=0.2,Q=0.2)
+
+comp_node = CompositeNode(CurrentNodes=[CSI, CSI2], PowerNodes=[PQ, PQ2], VoltageNode=VSI)
+
+typeof(comp_node)
+
+construct_vertex(comp_node)
+
+
 @testset "CompositeNode" begin
-    comp_node = CompositeNode([CSIMinimal(I_r = 1.0), CSIMinimal(I_r = 1.0im)])
+    # constant voltage
+    VSI = VSIMinimal(τ_P=1.,τ_Q=1.,K_P=1.,K_Q=1.,V_r=1.,P=1.,Q=1.)
+
+    # constant current
+    CSI = CSIMinimal(I_r=1.)
+    CSI2 = CSIMinimal(I_r=2.)
+
+    # constant power
+    PQ = PQAlgebraic(P=0.1,Q=0.1)
+    PQ2 = PQAlgebraic(P=0.2,Q=0.2)
+
+    comp_node = CompositeNode(CurrentNodes=[CSI, CSI2], PowerNodes=[PQ, PQ2], VoltageNode=VSI)
     comp_vertex = construct_vertex(comp_node)
 
     @test symbolsof(comp_node) == [:u_r, :u_i]
