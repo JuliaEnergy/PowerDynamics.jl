ef27300202d8e8b34682e05045a198e949723c13
Paul Schultz <pschultz@pik-potsdam.de>
2019-12-12 15:44:26 +0100

    jetzt aber

---
+++ src/nodes/experimental/DGUnit.jl
@@ -0,0 +1,22 @@
+@doc doc"""
+```Julia
+DGUnit(;I_r)
+```
+
+
+"""
+@DynamicNode DGUnit(; Pref, Qref) begin
+    MassMatrix()
+end  begin
+end [[Pmeas, dPmeas], [Qmeas, dQmeas]] begin
+    # saturation
+    # time delay
+
+    Va_amp = abs(u)
+    Va_phase = angle(u)
+    I_r = 2//3 * conj(Pref + im * Qref) / Va_amp
+    du = i - I_r
+    dPmeas = 0.
+end
+
+export CSIMinimal
