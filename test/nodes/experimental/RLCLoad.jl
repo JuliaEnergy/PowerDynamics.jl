732a0700c2ac587fae6eee466f906705a6c7275e
Sabine Auer <sabine.auer@elena-international.com>
2019-12-13 17:15:36 +0100

    bug in RLC load fixed

--- test/nodes/experimental/RLCLoad.jl
+++ test/nodes/experimental/RLCLoad.jl
@@ -6,12 +6,12 @@ include("../NodeTestBase.jl")
 
 @testset "RLCLoad" begin
     R,L,C = rand_positive(3)
-    u_C,i_L,omega,domega,du_C,di_L = rand_real(6)
+    u_Cr, u_Ci,i_Lr,i_Li,du_Cr,du_Ci,di_Lr,di_Li,omega,domega  = rand_real(10)
 
     pv = RLCLoad(R=R, L=L, C=C)
     pv_vertex = construct_vertex(pv)
-    @test symbolsof(pv) == [:u_r, :u_i, :u_C, :i_L, :ω]
-    @test pv_vertex.mass_matrix == [0,0,1,1,1]
+    @test symbolsof(pv) == [:u_r, :u_i, :u_Cr, :u_Ci, :i_Lr,:i_Li, :ω]
+    @test pv_vertex.mass_matrix == [0,0,1,1,1,1,1]
 
-    smoketest_rhs(pv_vertex, int_x=[u_C, i_L, omega], int_dx=[domega, du_C, di_L])
+    smoketest_rhs(pv_vertex, int_x=[u_Cr, u_Ci, i_Lr,i_Li, omega], int_dx=[domega, du_Cr,du_Ci, di_Lr,di_Li])
 end
