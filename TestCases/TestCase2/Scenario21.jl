8a63d2be75b9669d506013e84b36909fa2af0dbd
Sabine Auer <sabine.auer@elena-international.com>
2020-01-23 12:10:24 +0100

    testcase 2 added

---
+++ TestCases/TestCase2/Scenario21.jl
@@ -0,0 +1,27 @@
+using Pkg
+Pkg.instantiate()
+using PowerDynamics
+using Revise
+
+perturbed_node=2
+
+node_list=[]
+    append!(node_list,[SlackAlgebraic(U=1.)])
+    append!(node_list,[PQAlgebraic(P=-0.5,Q=0.)])
+line_list=[]
+    append!(line_list,[StaticLine(from=1,to=2,Y=-1im/0.02)])
+
+powergrid = PowerGrid(node_list,line_list)
+operationpoint = find_operationpoint(powergrid)
+
+timespan = (0., 20.)
+pd = PowerPerturbation(
+    fraction = 0.7,
+    node_number = perturbed_node,
+    tspan_fault = (1.,10.))
+
+result_pd = simulate(pd,
+    powergrid, operationpoint, timespan)
+
+include("../../plotting.jl")
+plot_res(result_pd,powergrid,perturbed_node)
