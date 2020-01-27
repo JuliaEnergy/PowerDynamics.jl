b3a5961a251d4e35d4ef1e980b30eba9e9477bc2
Sabine Auer <sabine.auer@elena-international.com>
2020-01-27 12:53:59 +0100

    connector lines and nodes for dummy nodes added

---
+++ src/lines/ConnectorLine.jl
@@ -0,0 +1,17 @@
+"""
+```Julia
+StaticLine(from, to, Y)
+```
+A dummy line that represents a static line with an admittance of Y=0.
+For numeric reasons the line is not exactly zero but Z=1e-8+1e-8*j
+"""
+@Line ConnectorLine(from, to) begin
+    # If current is flowing away from the source, it is negative at the source.
+    # the current flowing in and out of the line is the same: I_mk=I_km
+    # hence, the current vector becomes only one complex current
+    Y = (1/(1e-8+1im*1e-8))# TODO
+    complex_current = Y * (destination_voltage - source_voltage)
+    current_vector = [complex_current,complex_current]
+end
+
+export ConnectorLine
---
+++ src/nodes/experimental/Connector.jl
@@ -0,0 +1,29 @@
+# (C) 2018 Potsdam Institute for Climate Impact Research, authors and contributors (see AUTHORS file)
+# Licensed under GNU GPL v3 (see LICENSE file)
+
+@doc doc"""
+```Julia
+PQAlgebraic(;S)
+```
+
+A node type that locally fixes the active (``P``) and reactive power (``Q``) output of the node.
+
+# Keyword Arguments
+- `S = P + Q*im`: the complex power output
+
+# Mathematical Representation
+Using `PQAlgebraic` for node ``a`` applies the equation
+```math
+0 = S_a - u_a \cdot i_a^*.
+```
+"""
+@DynamicNode Connector() begin
+    MassMatrix()
+end  begin
+    # no prep
+end [] begin
+    s = u*conj(i)
+    du = s-(-1e-8-1e-8*1im)
+end
+
+export Connector
