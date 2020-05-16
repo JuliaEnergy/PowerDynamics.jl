614e58a87b92b75d110c6e8a6ed1159add2262cc
Paul Schultz <pschultz@pik-potsdam.de>
2020-05-16 14:36:26 +0200

    develop test

--- test/simulations/NodeShortCircuit.jl
+++ test/simulations/NodeShortCircuit.jl
@@ -1,30 +1,35 @@
 using Test: @test
-using PowerDynamics: simulate, PowerGrid, systemsize, rhs, PiModelLine, PQAlgebraic, VSIMinimal, StaticLine, NodeShortCircuit, find_valid_initial_condition
+using PowerDynamics:
+  simulate,
+  PowerGrid,
+  systemsize,
+  rhs,
+  CSIMinimal,
+  SlackAlgebraic,
+  StaticLine,
+  NodeShortCircuit,
+  find_valid_initial_condition
 using OrdinaryDiffEq: ODEProblem, Rodas4
 import DiffEqBase: solve
 
-busses = [VSIMinimal(;τ_P=1.0,τ_Q=0.0001,K_P=0.2,K_Q=0.002,V_r=1.0,P=1.0,Q=0.5),
-  VSIMinimal(;τ_P=1.0,τ_Q=0.0001,K_P=0.2,K_Q=0.002,V_r=1.0,P=1.0,Q=0.5),
-  VSIMinimal(;τ_P=1.0,τ_Q=0.0001,K_P=0.2,K_Q=0.002,V_r=1.0,P=1.0,Q=0.5),
-  PQAlgebraic(P=-1.0,Q=-im*0.5),
-  PQAlgebraic(P=-1.0,Q=-im*0.5),
-  PQAlgebraic(P=-1.0,Q=-im*0.5)]
-  #RLC_Load(R,L,C),RLC_Load(R,L,C),RLC_Load(R,L,C)]
+busses = [
+  SlackAlgebraic(; U = complex(1.0, 0.0)),
+  CSIMinimal(; I_r = complex(0.4988, -0.4988)),
+]
 
 
 lines = [
-  PiModelLine(;from=1, to=4, y = 1/(0.1152 + im*0.0458), y_shunt_km = 0.,  y_shunt_mk = 0.),
-  PiModelLine(;from=2, to=5, y = 1/(0.1152 + im*0.0458), y_shunt_km = 0.,  y_shunt_mk = 0.),
-  PiModelLine(;from=3, to=6, y = 1/(0.1152 + im*0.0458), y_shunt_km = 0.,  y_shunt_mk = 0.),
-  PiModelLine(;from=2, to=3, y = 1/(0.1152 + im*0.0458), y_shunt_km = 0.,  y_shunt_mk = 0.),
-  PiModelLine(;from=1, to=2, y = 1/(0.1152 + im*0.0458), y_shunt_km = 0.,  y_shunt_mk = 0.)]
+  StaticLine(; from = 1, to = 2, Y = complex(1.736, -208.326)),
+  PiModelLine(;
+    from = 1,
+    to = 2,
+    y = complex(1.736, -208.326),
+    y_shunt_km = 0.0,
+    y_shunt_mk = 0.0,
+  ),
+]
 
-stat_lines = [
-  StaticLine(;from=1, to=4, Y = 1/(0.1152 + im*0.0458)),
-  StaticLine(;from=2, to=5, Y = 1/(0.1152 + im*0.0458)),
-  StaticLine(;from=3, to=6, Y = 1/(0.1152 + im*0.0458)),
-  StaticLine(;from=2, to=3, Y = 1/(0.1152 + im*0.0458)),
-  StaticLine(;from=1, to=2, Y = 1/(0.1152 + im*0.0458))]
+
 
 nsc = NodeShortCircuit(node = 1, R = 0.1, sc_timespan=(1.,2.))
 
