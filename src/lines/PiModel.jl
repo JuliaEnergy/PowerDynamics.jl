2f9cc62c2f5d93b21302cec264f95f8ac7e73b77
Paul Schultz <pschultz@pik-potsdam.de>
2020-05-16 14:54:52 +0200

    updated docs for trafo and other lines

--- src/lines/PiModel.jl
+++ src/lines/PiModel.jl
@@ -1,11 +1,21 @@
+"""
+```Julia
+    PiModel(y, y_shunt_km, y_shunt_mk, t_km, t_mk)
+```
+Implementation of the unified branch model with our sign conventions.
+See also the Chapter 2 in
+  Göran Andersson, _Power System Analysis_, Lecture 227-0526-00, ITET ETH Zurich, 2012
+Assumptions:
+* the line admittance is symmetric
+
+# Arguments
+- `y`: line admittance
+- `y_shunt_km`: shunt admittance at the end connected to node `k`
+- `y_shunt_mk`: shunt admittance at the end connected to node `m`
+- `t_km`: transformer ratio at the end connected to node `k`
+- `t_mk`: transformer ratio at the end connected to node `m`
+"""
 function PiModel(y, y_shunt_km, y_shunt_mk, t_km, t_mk)
-    """
-    Implementation of the unified branch model with our sign conventions.
-    See also the Chapter 2 in
-      Göran Andersson, _Power System Analysis_, Lecture 227-0526-00, ITET ETH Zurich, 2012
-    Assumptions:
-    * the line admittance is symmetric
-    """
     Π = zeros(Complex{Float64}, 2, 2)
     Π[1, 1] = - abs2(t_km) * (y + y_shunt_km) # Our sign convention is opposite for the source of the edge
     Π[1, 2] = conj(t_km) * t_mk * y # Our sign convention is opposite for the source of the edge
