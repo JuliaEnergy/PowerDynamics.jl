f97af27d6b084291a04f9aaccd21a72d1b962e50
Paul Schultz <22936388+luap-pik@users.noreply.github.com>
2020-05-24 22:57:24 +0200

    Update Transformer.jl (#81)
    * Update Transformer.jl
    
    correct function signature
    
    * updated docs for trafo and other lines
    
    * added tests
    
    Co-authored-by: Sabine Auer <sabine.auer@elena-international.com>

--- src/lines/PiModelLine.jl
+++ src/lines/PiModelLine.jl
@@ -1,8 +1,23 @@
 """
 ```Julia
-PiModelLine(from, to, y, y_shunt_km, y_shunt_mk)
+    PiModelLine(from, to, y, y_shunt_km, y_shunt_mk)
 ```
 A line modelled according to the PI-Model.
+
+See also the Chapter 2 in
+  Göran Andersson, _Power System Analysis_, Lecture 227-0526-00, ITET ETH Zurich, 2012
+
+# Arguments
+- `from` : node `k`
+- `to` : node `m`
+- `y`: admittance of line between `k` and `m`
+- `y_shunt_km`: shunt admittance at the end connected to node `k`
+- `y_shunt_mk`: shunt admittance at the end connected to node `m`
+- `t_km`: transformer ratio at the end connected to node `k`
+- `t_mk`: transformer ratio at the end connected to node `m`
+
+# Assumptions:
+- the line admittance is symmetric
 """
 @Line PiModelLine(from, to, y, y_shunt_km, y_shunt_mk) begin
     Y = PiModel(y, y_shunt_km, y_shunt_mk, 1, 1)
