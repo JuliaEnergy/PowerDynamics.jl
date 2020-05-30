81644ed2908d08ad5c7a42fc1b912bba3586bb86
Paul Schultz <pschultz@pik-potsdam.de>
2020-05-30 21:50:21 +0200

    :rootfind works after fixing problem in SteadyStateDiffEq

--- test/operationpoint/operationpoint.jl
+++ test/operationpoint/operationpoint.jl
@@ -1,7 +1,41 @@
-using PowerDynamics: OperationPointError, find_operationpoint, rhs, RootRhs, SlackAlgebraic, SwingEqLVS, PQAlgebraic, StaticLine,
-PowerGrid, find_operationpoint, RootRhs, rhs, systemsize
-using LightGraphs: SimpleGraph
+using PowerDynamics
+#using PowerDynamics: OperationPointError, find_operationpoint, rhs, RootRhs, SlackAlgebraic, SwingEqLVS, PQAlgebraic, StaticLine, PowerGrid, find_operationpoint, RootRhs, rhs, systemsize, find_operationpoint
+using LightGraphs: SimpleGraph, add_edge!
 using Test: @test, @testset
+
+U1 = complex(1., 0.)
+P2 = -1.
+Q2= 0.
+Y = 20f0im
+nodes = [SlackAlgebraic(U=U1), PQAlgebraic(P=P2,Q=Q2)]
+lines = [StaticLine(from=1, to=2, Y=Y)]
+graph = SimpleGraph(2)
+add_edge!(graph, 1, 2)
+grid = PowerGrid(graph, nodes, lines)
+rpg = rhs(grid)
+PowerDynamics.guess.(grid.nodes, 5.)
+PowerDynamics.initial_guess(grid)
+systemsize(grid)
+op = find_operationpoint(grid)
+
+op = find_operationpoint(grid; method = :nlsolve)
+
+using OrdinaryDiffEq: ODEProblem, Rodas5
+using SteadyStateDiffEq
+
+op_prob = ODEProblem(rpg, PowerDynamics.initial_guess(grid), Inf)
+sol = solve(
+    SteadyStateProblem(op_prob),
+    DynamicSS(Rodas5()),
+)
+sol.retcode == :Success
+
+op2 = find_operationpoint(grid; method = :steadystate, abstol = 1e-6, reltol = 1e-6, tspan = Inf)
+
+all(op[:, :v] .≈ op2[:, :v])
+isapprox.(op[:, :φ], op2[:, :φ], atol=1e-8) |> all
+
+op3 = find_operationpoint(grid; method = :rootfind)
 
 
 @testset "algebraic only" begin
@@ -10,9 +44,9 @@ using Test: @test, @testset
     Q2= 3
     Y = 2+1.5im
     nodes = [SlackAlgebraic(U=U1), PQAlgebraic(P=P2,Q=Q2)]
-    graph = SimpleGraph(2)
-    add_edge!(graph, 1, 2);
     lines = [StaticLine(from=1, to=2, Y=Y)]
+    graph = SimpleGraph(2)
+    add_edge!(graph, 1, 2)
     grid = PowerGrid(graph, nodes, lines)
     op = find_operationpoint(grid)
     root = RootRhs(rhs(grid))
