a6ae2fd86541345d0b49fe8c8614f5fccfd78084
Sabine Auer <sabine.auer@elena-international.com>
2020-06-18 20:22:54 +0200

    testing

--- test_short-circuit.jl
+++ test_short-circuit.jl
@@ -6,22 +6,22 @@ using OrdinaryDiffEq: ODEProblem, Rodas4, solve;
 
 #include("helpers.jl");
 
-busses = [VSIMinimal(;τ_P=1.0,τ_Q=0.0001,K_P=0.2,K_Q=0.002,V_r=1.0,P=1.0,Q=0.5),
-  SwingEqLVS(H=1, P=1, D=1, Ω=2π*50,Γ=0.1,V=1.),
+busses = Dict("bus1"=>VSIMinimal(;τ_P=1.0,τ_Q=0.0001,K_P=0.2,K_Q=0.002,V_r=1.0,P=1.0,Q=0.5),
+  "bus2"=>SwingEqLVS(H=1, P=1, D=1, Ω=2π*50,Γ=0.1,V=1.))
   #VSIMinimal(;τ_P=1.0,τ_Q=0.0001,K_P=0.2,K_Q=0.002,V_r=1.0,P=1.0,Q=0.5),
-  VSIMinimal(;τ_P=1.0,τ_Q=0.0001,K_P=0.2,K_Q=0.002,V_r=1.0,P=1.0,Q=0.5),
-  PQAlgebraic(P=-1.0,Q=-0.5),
-  PQAlgebraic(P=-1.0,Q=-0.5),
-  PQAlgebraic(P=-1.0,Q=-0.5)];
+  #VSIMinimal(;τ_P=1.0,τ_Q=0.0001,K_P=0.2,K_Q=0.002,V_r=1.0,P=1.0,Q=0.5),
+  #PQAlgebraic(P=-1.0,Q=-0.5),
+  #PQAlgebraic(P=-1.0,Q=-0.5),
+  #PQAlgebraic(P=-1.0,Q=-0.5)];
   #RLC_Load(R,L,C),RLC_Load(R,L,C),RLC_Load(R,L,C)]
 
 
-lines = [
-  PiModelLine(;from=1, to=4, y = 1/(0.1152 + im*0.0458), y_shunt_km = 0.,  y_shunt_mk = 0.),
-  PiModelLine(;from=2, to=5, y = 1/(0.1152 + im*0.0458), y_shunt_km = 0.,  y_shunt_mk = 0.),
-  PiModelLine(;from=3, to=6, y = 1/(0.1152 + im*0.0458), y_shunt_km = 0.,  y_shunt_mk = 0.),
-  PiModelLine(;from=2, to=3, y = 1/(0.1152 + im*0.0458), y_shunt_km = 0.,  y_shunt_mk = 0.),
-  PiModelLine(;from=1, to=2, y = 1/(0.1152 + im*0.0458), y_shunt_km = 0.,  y_shunt_mk = 0.)];
+lines = Dict(
+"line1"=> PiModelLine(;from=1, to=4, y = 1/(0.1152 + im*0.0458), y_shunt_km = 0.,  y_shunt_mk = 0.),
+"line2"=>  PiModelLine(;from=2, to=5, y = 1/(0.1152 + im*0.0458), y_shunt_km = 0.,  y_shunt_mk = 0.))
+#  PiModelLine(;from=3, to=6, y = 1/(0.1152 + im*0.0458), y_shunt_km = 0.,  y_shunt_mk = 0.),
+#  PiModelLine(;from=2, to=3, y = 1/(0.1152 + im*0.0458), y_shunt_km = 0.,  y_shunt_mk = 0.),
+#  PiModelLine(;from=1, to=2, y = 1/(0.1152 + im*0.0458), y_shunt_km = 0.,  y_shunt_mk = 0.)];
 
 
 pg = PowerGrid(busses, lines);
