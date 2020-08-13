13bcdbddedb95ffdf23d3ef67a311f02a260c0f6
Frank Hellmann <hellmann@pik-potsdam.de>
2020-08-13 22:04:09 +0200

    Composite node (#75)
    * Started to work on CompositeCurrentNodes
    
    * Tests passing for CompositeNode
    
    * Started drafting the full Composite Node design...
    
    * composite types
    
    * First Draft of full Composite Node (supporting voltages and powers) ready for more thorough testing.
    
    * Test fixed. All tests passing.
    
    * small edits, sympy tests
    
    * addedchangelog entry
    
    * save state
    
    * merging master and fixing tests
    
    * merging master and fixing tests
    
    * moved CompositeNode to experimental folder
    
    * fix file not found
    
    * runexample added to try out composite node, simulation does not run through, yet
    
    * imported simulate funtion
    
    * run script works
    
    Co-authored-by: Paul Schultz <pschultz@pik-potsdam.de>
    Co-authored-by: Paul Schultz <22936388+luap-pik@users.noreply.github.com>
    Co-authored-by: Sabine Auer <sabine.auer@elena-international.com>

---
+++ run_compositenode.jl
@@ -0,0 +1,41 @@
+using PowerDynamics: CompositeNode, CSIMinimal, VSIMinimal, PQAlgebraic, construct_vertex, symbolsof, simulate,SlackAlgebraic,PiModelLine,PowerGrid,find_operationpoint
+using PowerDynamics: PowerPerturbation,PQAlgebraic
+# constant voltage
+#VSI = VSIMinimal(τ_P=1.,τ_Q=1.,K_P=1.,K_Q=1.,V_r=1.,P=1.,Q=1.)
+
+τ_P, τ_Q, K_P, K_Q,V_r = ones(5)
+P,Q = rand(2)
+VSI = VSIMinimal(τ_P=τ_P,τ_Q=τ_Q,K_P=K_P,K_Q=K_Q,V_r=V_r,P=P,Q=Q)
+
+# constant current
+I_r = rand(ComplexF64)
+CSI = CSIMinimal(I_r=I_r)
+CSI2 = CSIMinimal(I_r=I_r)
+
+# constant power
+P,Q=rand(2)
+PQ = PQAlgebraic(P=P,Q=Q)
+PQ2 = PQAlgebraic(P=P,Q=Q)
+
+# Test vertex with voltage dynamic
+comp_node = CompositeNode(CurrentNodes=[CSI, CSI2], PowerNodes=[PQ, PQ2], VoltageNode=VSI)
+comp_vertex = construct_vertex(comp_node)
+
+node_list=[
+    SlackAlgebraic(U=1),
+    PQAlgebraic(P=1,Q=1),
+    comp_node]
+
+line_list=[
+    PiModelLine(from=1, to=2, y=4.99913-15.2631im, y_shunt_km=1/2*0.0528*1im, y_shunt_mk=1/2*0.0528*1im),
+    PiModelLine(from=2, to=3, y=4.99913-15.2631im, y_shunt_km=1/2*0.0528*1im, y_shunt_mk=1/2*0.0528*1im)]
+
+final_time=5.
+timespan= (0.,final_time);
+disturbed_node=2;
+powergrid = PowerGrid(node_list, line_list);
+operationpoint = find_operationpoint(powergrid);
+pd = PowerPerturbation(node_number=disturbed_node, fraction=1.1, tspan_fault=(0.01,0.5));
+#include("plotting2.jl");
+
+sol_pd = simulate(pd, powergrid, operationpoint, timespan)
--- src/PowerDynamics.jl
+++ src/PowerDynamics.jl
@@ -33,6 +33,7 @@ include("nodes/experimental/PVInverterWithFrequencyControl.jl")
 include("nodes/experimental/WindTurbineGenType4.jl")
 include("nodes/experimental/WindTurbineGenType4_RotorControl.jl")
 include("nodes/experimental/CurtailedPowerPlantWithInertia.jl")
+include("nodes/experimental/CompositeNode.jl")
 
 # all line types
 
