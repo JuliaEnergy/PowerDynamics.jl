ec676cbd59eb55afbb7900194a5cb8cc832dffd6
Paul Schultz <22936388+luap-pik@users.noreply.github.com>
2020-08-13 12:05:05 +0200

    renamed constants

--- src/nodes/VoltageDependentLoad.jl
+++ src/nodes/VoltageDependentLoad.jl
@@ -3,7 +3,7 @@
 
 @doc doc"""
 ```Julia
-    VoltageDependentLoad(;P, Q, U, q, l)
+    VoltageDependentLoad(;P, Q, U, A, B)
 ```
 
 A node type that locally fixes the active (``P``) and reactive power (``Q``) output of the node.
@@ -12,24 +12,24 @@ A node type that locally fixes the active (``P``) and reactive power (``Q``) out
 - `P`: active power demand
 - `Q`: reactive power demand
 - `U` : the voltage set point
-- `q` : relative share of quadratic voltage dependence 
-- `l` : relative share of linear voltage dependence 
+- `A` : relative share of quadratic voltage dependence 
+- `B` : relative share of linear voltage dependence 
 
 # Mathematical Representation
 Using `VoltageDependentLoad` for node ``a`` applies the equation
 ```math
-0 = S_a ( q (u_a/U_a)^2 + l (u_a/U_a) + 1 - q - l) - u_a \cdot i_a^*.
+0 = S_a ( A * (u_a/U_a)^2 + B * (u_a/U_a) + 1 - A - B) - u_a \cdot i_a^*.
 ```
 """
-@DynamicNode VoltageDependentLoad(P, Q, U, q, l) begin
+@DynamicNode VoltageDependentLoad(P, Q, U, A, B) begin
     MassMatrix()
 end  begin
-    @assert 0 <= q <= 1
-    @assert 0 <= l <= 1
+    @assert 0 <= A <= 1
+    @assert 0 <= B <= 1
 end [] begin
     s = u * conj(i)
     u_rel = u / U
-    du = complex(P, Q) * (q * u_rel^2 + l * u_rel + 1 - q - l) - s
+    du = complex(P, Q) * (A * u_rel^2 + B * u_rel + 1 - A - B) - s
 end
 
 export VoltageDependentLoad
