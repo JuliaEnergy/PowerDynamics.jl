83a837c238dffc5a34917395853f59b7feac721b
Aleksandra Lekic-Vervoort <alekic@tudelft.net>
2020-08-17 20:44:40 +0200

    Update power_flow.jl
    Nodes start: SlackAlgebraic, PVAlgebraic and PQAlgebraic.

--- src/operationpoint/power_flow.jl
+++ src/operationpoint/power_flow.jl
@@ -10,6 +10,61 @@ function power_flow(power_grid :: PowerGrid)
     ang_min = deg2rad(360)
     ang_max = deg2rad(-360)
 
+    function make_bus_ac(data :: Dict{String, Any}, key_e :: Int,  node)
+        key_e = length(data["bus"])+1
+        (data["bus"])[string(key_e)] = Dict{String, Any}()
+        dict = (data["bus"])[string(key_e)]
+        dict["source_id"] = Any["bus", key_e]
+        dict["index"] = key_e
+        dict["bus_i"] = key_e
+        dict["zone"] = 1
+        dict["area"] = 1
+        dict["vmin"] = 0.9
+        dict["vmax"] = 1.1
+        dict["vm"] = 1
+        dict["va"] = 0
+        dict["base_kv"] = global_dict["V"] / 1e3
+
+        if isa(node, PQAlgebraic)
+            dict["bus_type"] = 1
+
+            key_l = length(data["load"]) + 1
+            (data["load"])[string(key_l)] = Dict{String, Any}()
+            dict_l = (data["load"])[string(key_l)]
+            dict_l["source_id"] = dict["source_id"]
+            dict_l["load_bus"] = key_e
+            dict_l["status"] = 1
+            dict_l["pd"] = node.P
+            dict_l["qd"] = node.Q
+            dict_l["index"] = key_l
+        elseif isa(node, PVAlgebraic)
+            dict["bus_type"] = 2
+            dict["vm"] = abs(node.V)  # assumed p.u.
+            dict["vmin"] = 0.9 * dict["vm"]
+            dict["vmax"] = 1.1 * dict["vm"]
+            dict["va"] = angle(node.V)
+
+            key_l = length(data["load"]) + 1
+            (data["load"])[string(key_l)] = Dict{String, Any}()
+            dict_l = (data["load"])[string(key_l)]
+            dict_l["source_id"] = dict["source_id"]
+            dict_l["load_bus"] = key_e
+            dict_l["status"] = 1
+            dict_l["pd"] = node.P
+            dict_l["qd"] = 0
+            dict_l["index"] = key_l
+        elseif isa(node, SlackAlgebraic)
+            dict["bus_type"] = 3
+            dict["vm"] = abs(node.U)  # assumed p.u.
+            dict["vmin"] = 0.9 * dict["vm"]
+            dict["vmax"] = 1.1 * dict["vm"]
+            dict["va"] = angle(node.U)
+        else
+            dict["bus_type"] = 4
+        end
+
+    end
+
     function make_branch_ac(data :: Dict{String, Any}, key_e :: Int,  line)
         data["f_bus"] = line.from
         data["t_bus"] = line.to
@@ -68,6 +123,7 @@ function power_flow(power_grid :: PowerGrid)
 
 
 
+
     data = Dict{String, Any}()
     data["source_type"] = "matpower"
     data["name"] = "network"
@@ -87,6 +143,15 @@ function power_flow(power_grid :: PowerGrid)
         (data["branch"])[string(key_e)] = Dict{String, Any}()
         make_branch_ac((data["branch"])[string(key_e)], key_e, line)
     end
+    for node in power_grid.nodes
+        make_bus_ac(data, key_e, node)
+    end
+
+    PowerModelsACDC.process_additional_data!(data)
+    ipopt = JuMP.optimizer_with_attributes(Ipopt.Optimizer, "tol" => 1e-6, "print_level" => 0)
+    s = Dict("output" => Dict("branch_flows" => true), "conv_losses_mp" => false)
+
+    result = run_acdcpf(data, ACPPowerModel, ipopt; setting = s)
 
     return data
 end
