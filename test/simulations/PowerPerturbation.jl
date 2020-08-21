35d2222787645e4c2788d0d2fe814ca5c67d8d49
Paul Schultz <pschultz@pik-potsdam.de>
2020-08-21 09:48:38 +0200

    small edit

--- test/simulations/PowerPerturbation.jl
+++ test/simulations/PowerPerturbation.jl
@@ -1,20 +1,17 @@
 using Test: @test,@testset, @test_throws
 using PowerDynamics: PowerPerturbation, simulate, SwingEqLVS, SlackAlgebraic, StaticLine, PowerGrid, State,systemsize, FieldUpdateError
 
-#@testset "test PowerPerturbation simulation" begin
-nodes = [SwingEqLVS(H=1., P=-1.0, D=1, Ω=50, Γ=20, V=1), SwingEqLVS(H=1., P=1.0, D=1, Ω=50, Γ=20, V=1)]
-lines = [StaticLine(Y=-50*im, from=1, to=2)]
-grid = PowerGrid(nodes, lines)
-state = State(grid, rand(systemsize(grid)))
+@testset "test PowerPerturbation simulation" begin
+    nodes = [SwingEqLVS(H=1., P=-1.0, D=1, Ω=50, Γ=20, V=1), SwingEqLVS(H=1., P=1.0, D=1, Ω=50, Γ=20, V=1)]
+    lines = [StaticLine(Y=-50*im, from=1, to=2)]
+    grid = PowerGrid(nodes, lines)
+    state = State(grid, rand(systemsize(grid)))
 
-sol = simulate(PowerPerturbation(
-    node = 1,
-    power_new = -0.9,
-    tspan_fault = (0.1,1.)),
-    state, (0., 1.))
-@test sol !== nothing
+    pp = PowerPerturbation(node = 1, power_new = -0.9, tspan_fault = (0.1,1))
+    sol = simulate(pp, state, (0., 1.))
+    @test sol !== nothing
 @test sol.dqsol.retcode == :Success
-#end
+end
 
 @testset "test PowerPerturbation simulation P of type Int should also be accepted" begin
     invalidP = 0.9
