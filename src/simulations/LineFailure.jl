479d27bea149336803dfc9fa3c6aca2366482313
Sabine Auer <sabine.auer@elena-international.com>
2020-08-27 13:41:15 +0200

    added LineFailure tests, removing deps, restoring LineFault

--- src/simulations/LineFailure.jl
+++ src/simulations/LineFailure.jl
@@ -1,20 +1,21 @@
 """
 ```Julia
-LineFailure(;from,to)
+LineFault(;from,to)
 ```
 The arguments `from` and `to` specify the line that should be disconnected from the grid.
 """
-struct LineFailure
+struct LineFault
     from
     to
-    LineFailure(; from=from, to=to) = new(from, to)
+    LineFault(; from=from, to=to) = new(from, to)
 end
 
-struct LineFailure_new
+struct LineFailure
     line_name
+    LineFailure(; line_name=line_name) = new(line_name)
 end
 
-function (lf::LineFailure)(powergrid)
+function (lf::LineFault)(powergrid)
     @assert lf.from < lf.to "order important to remove the line from powergrid"
     filtered_lines = filter(l -> (l.from !=lf.from || l.to !=lf.to), copy(powergrid.lines))
     PowerGrid(powergrid.nodes, filtered_lines)
@@ -28,7 +29,7 @@ function filter_lines(lines::Array, line_name)
     copy(lines[1:end .!= line_name])
 end
 
-function (lf::LineFailure_new)(powergrid)
+function (lf::LineFailure)(powergrid)
     filtered_lines=filter_lines(powergrid.lines, lf.line_name)
     PowerGrid(powergrid.nodes, filtered_lines)
 end
@@ -36,23 +37,23 @@ end
 
 """
 ```Julia
-simulate(lf::LineFailure, powergrid, x0; timespan)
+simulate(lf::LineFault, powergrid, x0; timespan)
 ```
-Simulates a [`LineFailure`](@ref)
+Simulates a [`LineFault`](@ref)
 """
-function simulate(lf::LineFailure, powergrid, x0; timespan)
+function simulate(lf::LineFault, powergrid, x0; timespan)
     solve(lf(powergrid), x0, timespan);
 end
 
-function simulate(lf::LineFailure_new, powergrid, x0, timespan)
+function simulate(lf::LineFailure, powergrid, x0, timespan)
     solve(lf(powergrid), x0, timespan);
 end
 
-function simulate(lf::LineFailure, x0::State; timespan)
+function simulate(lf::LineFault, x0::State; timespan)
     solve(lf(x0.grid), x0.vec, timespan);
 end
 
-function simulate(lf::LineFailure_new, x0::State, timespan)
+function simulate(lf::LineFailure, x0::State, timespan)
     solve(lf(x0.grid), x0.vec, timespan);
 end
 
@@ -62,5 +63,5 @@ const iipfunc = true # is in-place function
 #LineFailure(;from,to) = LineFailure(from,to)
 
 
+export LineFault
 export LineFailure
-export LineFailure_new
--- test/simulations/LineFailure.jl
+++ test/simulations/LineFailure.jl
@@ -1,14 +1,26 @@
 using Test: @test
 using LightGraphs: edges, Edge
-using PowerDynamics: SlackAlgebraic, SwingEqLVS, StaticLine, LineFailure, simulate
+using PowerDynamics: SlackAlgebraic, SwingEqLVS, StaticLine, LineFailure,LineFault, simulate, PowerGrid
+using OrderedCollections: OrderedDict
 
 Y = 0 + 5*im
 nodes = [SlackAlgebraic(U=1), SwingEqLVS(H=1, P=-1, D=1, Ω=50, Γ=20, V=1), SwingEqLVS(H=1, P=-1, D=1, Ω=50, Γ=20, V=1)]
-lines = [StaticLine(from=e.src, to=e.dst, Y=Y) for e in edges(graph)]
+lines = [StaticLine(from=1, to=2, Y=Y),StaticLine(from=2, to=3, Y=Y)]
 grid = PowerGrid(nodes, lines)
 
-linefailure = LineFailure(;from=1, to=2)
+linefault = LineFault(;from=1, to=2)
+faulty_grid = linefault(grid)
+
+@test length(faulty_grid.lines) == 1
+@test collect(edges(faulty_grid.graph)) == [Edge(2,3)]
+
+Y = 0 + 5*im
+nodes = OrderedDict("bus1"=>SlackAlgebraic(U=1), "bus2"=>SwingEqLVS(H=1, P=-1, D=1, Ω=50, Γ=20, V=1), "bus3"=>SwingEqLVS(H=1, P=-1, D=1, Ω=50, Γ=20, V=1))
+lines = OrderedDict("line1"=>StaticLine(from="bus1", to="bus2", Y=Y),"line2"=>StaticLine(from="bus2", to="bus3", Y=Y))
+grid = PowerGrid(nodes, lines)
+
+linefailure = LineFailure(;line_name="line1")
 faulty_grid = linefailure(grid)
 
 @test length(faulty_grid.lines) == 1
-@test collect(edges(faulty_grid.graph)) == [Edge(1,3)]
+@test collect(edges(faulty_grid.graph)) == [Edge(2,3)]
