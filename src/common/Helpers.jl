062d958257e9290996bac575d050c7a0548b1c09
lindnemi <michaellindner@pik-potsdam.de>
2021-03-01 11:46:26 +0100

    adjusting to ND convention

--- src/common/Helpers.jl
+++ src/common/Helpers.jl
@@ -1,7 +1,7 @@
 function total_current(edges)
-    # Keeping with the convention of negative sign for outging current
+    # ND convention: all flows entering a node are positive
     current = 0.0im
-    for e in edges
+    @inbounds for e in edges
         current += e[1] + e[2]*im
     end
     current
