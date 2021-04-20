41037b60b495dcd117644e51a24f9643ae4fb6ec
Anton Plietzsch <plietzsch@pik-potsdam.de>
2021-04-20 19:40:40 +0200

    use arrays for ieee14 solution tests

--- test/examples/ieee14bus.jl
+++ test/examples/ieee14bus.jl
@@ -17,8 +17,8 @@ end
 
 operationpoint = find_operationpoint(pg_build)
 timespan= (0.0,5.)
-tsteps = 0.0:0.5:5.0
-rtol = 1e-4
+tsteps = 0.0:0.1:5.0
+rtol = 1e-3
 
 fault1 = ChangeInitialConditions(node="bus1", var=:ω, f=Inc(0.2))
 fault2 = LineFailure(line_name="branch2", tspan_fault=(1.,5.))
@@ -27,13 +27,13 @@ fault3 = PowerPerturbation(node="bus5", fault_power=0.0, tspan_fault=(1.,5.), va
 @testset "IEEE 14-bus solution tests" begin
     solution1 = simulate(fault1, pg_build, operationpoint, timespan)
     @load "./examples/ieee14sol1.jld2" ieee14sol1
-    @test isapprox(solution1(tsteps,:,:u), ieee14sol1(tsteps,:,:u), rtol=rtol)
+    @test isapprox(solution1(tsteps,:,:u), ieee14sol1, rtol=rtol)
 
     solution2 = simulate(fault2, pg_build, operationpoint, timespan)
     @load "./examples/ieee14sol2.jld2" ieee14sol2
-    @test isapprox(solution2(tsteps,:,:u), ieee14sol2(tsteps,:,:u), rtol=rtol)
+    @test isapprox(solution2(tsteps,:,:u), ieee14sol2, rtol=rtol)
 
     solution3 = simulate(fault3, pg_build, operationpoint, timespan)
     @load "./examples/ieee14sol3.jld2" ieee14sol3
-    @test isapprox(solution3(tsteps,:,:u), ieee14sol3(tsteps,:,:u), rtol=rtol)
+    @test isapprox(solution3(tsteps,:,:u), ieee14sol3, rtol=rtol)
 end
