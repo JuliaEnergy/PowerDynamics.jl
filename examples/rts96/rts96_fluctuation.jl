5243edaa8c07b9ee3b5112f1a16909111b9d28b2
Anton Plietzsch <plietzsch@pik-potsdam.de>
2021-10-07 12:29:00 +0200

    comments and bug fixes

--- examples/rts96/rts96_fluctuation.jl
+++ examples/rts96/rts96_fluctuation.jl
@@ -1,3 +1,10 @@
+#= 
+This is an example script for simulating fluctuations. For this we can use the node type
+FluctuationNode to model a bus with time varying infeed of active and reactive power. In
+this example script we provide a time series for renewable power fluctuations and convert
+it into a time dependend function by applying an interpolation.
+=#
+
 using CSV
 using DataFrames
 using LightGraphs
@@ -10,13 +17,13 @@ using GLMakie, GraphMakie, ColorSchemes, NetworkLayout
 
 ## Load Network Structure and Parameters from Data
 
-BusData = CSV.read("./examples/ieee14bus/Data/Bus.csv", DataFrame; types = [Int, Int, String, Int, Int])
-LineData = CSV.read("./examples/ieee14bus/Data/Line.csv",DataFrame)
+BusData = CSV.read("./examples/rts96/Data/Bus.csv", DataFrame; types = [Int, Int, String, Int, Int])
+LineData = CSV.read("./examples/rts96/Data/Line.csv",DataFrame)
     LineData[!, :source] = LineData.source .|> Int
     LineData[!, :dest] = LineData.dest .|> Int
-GeneratorData = CSV.read("./examples/ieee14bus/Data/Generator.csv",DataFrame)
-LoadData = CSV.read("./examples/ieee14bus/Data/Load.csv",DataFrame)
-FlowData = CSV.read("./examples/ieee14bus/Data/Flow.csv",DataFrame)
+GeneratorData = CSV.read("./examples/rts96/Data/Generator.csv",DataFrame)
+LoadData = CSV.read("./examples/rts96/Data/Load.csv",DataFrame)
+FlowData = CSV.read("./examples/rts96/Data/Flow.csv",DataFrame)
 
 const N = nrow(BusData)
 const L = nrow(LineData)
@@ -69,10 +76,16 @@ pg = PowerGrid(g, nodes, lines)
 ## power flow solution
 
 data, result = power_flow(pg);
-v = [result["solution"]["bus"][string(k)]["vm"] for k in 1:length(pg.nodes)]
-va = [result["solution"]["bus"][string(k)]["va"] for k in 1:length(pg.nodes)]
+v = [result["solution"]["bus"][string(k)]["vm"] for k in 1:length(pg.nodes)];
+va = [result["solution"]["bus"][string(k)]["va"] for k in 1:length(pg.nodes)];
 
 ## exchange slack with swing equation
+
+#=
+We exchange the slack bus by a dynamic generator model with the same power dispatch.
+This is gives a more realistic behavior of the dynamics, since a slack node would just
+swallow any power imbalances instantaniously.
+=#
 
 Hs = node_df[slack_idx,:].Inertia
 gen_idx = [gen for (gen,val) in data["gen"] if val["gen_bus"] == slack_idx][1]
@@ -89,23 +102,20 @@ x0 = State(pg, ic_guess);
 
 ## simulate the system
 
-tspan = (0.0, 100.0)
-ω_idx = findall(:ω .∈ symbolsof.(pg.nodes));
+# tspan = (0.0, 100.0)
+# ω_idx = findall(:ω .∈ symbolsof.(pg.nodes));
 
-ode = ODEProblem(rhs(pg), x0.vec, tspan);
-dqsol = solve(ode, Rodas4());
-sol = PowerGridSolution(dqsol, pg);
+# ode = ODEProblem(rhs(pg), x0.vec, tspan);
+# dqsol = solve(ode, Rodas4());
+# sol = PowerGridSolution(dqsol, pg);
 
-## plot results
+# ## plot results (check fixed point)
 
-Plots.plot(sol, :, :v, legend=false, xlim=(1e-3, 100.), xscale=:log10, ylabel="Vm")
-Plots.plot(sol, :, :φ, legend=false, xlim=(1e-3, 100.), xscale=:log10, ylabel="Va")
-Plots.plot(sol, ω_idx, :ω, legend=false, xlim=(1e-3, 100.), ylabel="\\omega")
+# Plots.plot(sol, :, :v, legend=false, xlim=(1e-3, 100.), xscale=:log10, ylabel="Vm")
+# Plots.plot(sol, :, :φ, legend=false, xlim=(1e-3, 100.), xscale=:log10, ylabel="Va")
+# Plots.plot(sol, ω_idx, :ω, legend=false, xlim=(1e-3, 100.), ylabel="\\omega")
 
 ## fluctuation time series
-
-loads = [idx for (idx,val) in enumerate(pg.nodes) if typeof(val) == PQAlgebraic];
-gens = [idx for (idx,val) in enumerate(pg.nodes) if typeof(val) == SwingEqLVS];
 
 function time_interpolation(series,Δt)
   itp = interpolate(series, BSpline(Linear()))
@@ -113,7 +123,7 @@ function time_interpolation(series,Δt)
   t -> etp(t/Δt .+1)
 end
 
-series = h5open("./examples/ieee14bus/fluctuations_time_series.h5","r") do file
+series = h5open("./examples/rts96/fluctuations_time_series.h5","r") do file
     read(file, "sum_of_fluctuations")
 end
 
@@ -121,19 +131,25 @@ Fluct = time_interpolation(series,0.01)
 
 ## simulate flucutation
 
+loads = [idx for (idx,val) in enumerate(pg.nodes) if typeof(val) == PQAlgebraic];
+gens = [idx for (idx,val) in enumerate(pg.nodes) if typeof(val) == SwingEqLVS];
+
 t_idx = Float64[]
+
+#= 
+We simulate single node fluctuations for every load node in the grid.
+=#
 
 for bus in loads
 
-    acp = nodes[bus].P
-    rep = nodes[bus].Q
+    acp = nodes[bus].P # active power
+    rep = nodes[bus].Q # reactive power
 
+    # add fluctuation to the load (exchange PQAlgebraic with FluctuationNode)
     nodes[bus] = FluctuationNode(t -> acp + 0.2*Fluct(t), t -> rep)
     pg_dynamic = PowerGrid(nodes, lines)
 
     timespan = (-5.,50.)
-    #fault = ChangeInitialConditions(node="bus2", var=:ω, f=Inc(0.0)) # dummy fault
-    #sol = simulate(fault, pg_dynamic, x0.vec, timespan)
     ode = ODEProblem(rhs(pg), x0.vec, tspan)
     dqsol = solve(ode, Rodas4())
     sol = PowerGridSolution(dqsol, pg);
@@ -147,7 +163,7 @@ for bus in loads
 
 end
 
-## plot fluctuation
+## plot fluctuation (using GraphMakie)
 
 include("plotexample.jl")
 dg, nlabels, arrow_size, node_color, node_shape = flow_plot(x0,t_idx)
