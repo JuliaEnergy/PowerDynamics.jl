768ea1cfbb3c61ce8ba652d1f1db88b2f38dd642
Anton Plietzsch <plietzsch@pik-potsdam.de>
2021-10-22 11:49:39 +0200

    update localmake.jl

--- docs/localmake.jl
+++ docs/localmake.jl
@@ -6,8 +6,6 @@ using Pkg
 Pkg.activate(@__DIR__)
 Pkg.develop(PackageSpec(path=dirname(@__DIR__))) # adds the package this script is called from
 push!(LOAD_PATH, Base.Filesystem.abspath("../"))
-using Documenter
-using PowerDynamics
 
 include("make.jl")
 
