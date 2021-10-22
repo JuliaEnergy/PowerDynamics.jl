6849d86f2dc31e014b3cc05908c6e30f4754f699
antonplietzsch <45694947+antonplietzsch@users.noreply.github.com>
2021-10-22 11:32:02 +0200

    Update docs/localmake.jl
    Co-authored-by: Hans Würfel <git@wuerfel.io>

--- docs/localmake.jl
+++ docs/localmake.jl
@@ -9,24 +9,6 @@ push!(LOAD_PATH, Base.Filesystem.abspath("../"))
 using Documenter
 using PowerDynamics
 
-makedocs(
-    modules = [PowerDynamics],
-    authors = "Tim Kittel, Jan Liße, Sabine Auer and further contributors.",
-    linkcheck=false,
-    sitename = "PowerDynamics.jl",
-    pages = [
-        "General" => "index.md",
-        "Architecture" => "ARCHITECTURE.md",
-        "language_conventions.md",
-        "powergrid_model.md",
-        "node_types.md",
-        "custom_node_types.md",
-        "line_types.md",
-        "states_solutions.md",
-        "simulations.md",
-        "error_types.md",
-        "import_export.md",
-        "fullindex.md",
-        "contact.md",
-    ]
-   )
+include("make.jl")
+
+using LiveServer; serve(dir=joinpath(@__DIR__, "build"))
