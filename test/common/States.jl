7b1e4a7b38b4ce2d52da0d4d951d71131f3bf6c4
Anton Plietzsch <plietzsch@pik-potsdam.de>
2022-06-17 11:25:34 +0200

    change order of tests to avoid numerical errors

--- test/common/States.jl
+++ test/common/States.jl
@@ -151,18 +151,6 @@ state_from_dict = State(grid_fromdict, [real(u_Sl), imag(u_Sl), real(u_Sw1), ima
 @test state_from_dict["bus2", :φ] ≈ φ_Sw1
 @test state_from_dict["bus3", :φ] ≈ φ_Sw2
 
-# exchange phi
-state[2, :φ] = φ_Sw2
-@test state[2:3, :φ] ≈ [φ_Sw2, φ_Sw2]
-state[2:3, :φ] = [φ_Sw1, φ_Sw2]
-@test state[2:3, :φ] ≈ [φ_Sw1, φ_Sw2]
-
-state_from_dict["bus2", :φ] = φ_Sw2
-@test state_from_dict[["bus2","bus3"], :φ] ≈ [φ_Sw2, φ_Sw2]
-state_from_dict[["bus2","bus3"], :φ] = [φ_Sw1, φ_Sw2]
-@test state_from_dict[["bus2","bus3"], :φ] ≈ [φ_Sw1, φ_Sw2]
-
-# test currents and powers
 i_1 = -Y * (u_Sw1 - u_Sl) -Y * (u_Sw2 - u_Sl)
 i_2 = Y * (u_Sw1 - u_Sl)
 i_3 = Y * (u_Sw2 - u_Sl)
@@ -191,6 +179,17 @@ s_2 =  state_from_dict["bus2", :u] * conj(i_2)
 @test state_from_dict[:, :s] ==  state[:, :u] .* conj.([i_1, i_2, i_3])
 @test state_from_dict[:, :p] ==  real(state[:, :u] .* conj.([i_1, i_2, i_3]))
 @test state_from_dict[:, :q] ==  imag(state[:, :u] .* conj.([i_1, i_2, i_3]))
+
+# exchange phi
+state[2, :φ] = φ_Sw2
+@test state[2:3, :φ] ≈ [φ_Sw2, φ_Sw2]
+state[2:3, :φ] = [φ_Sw1, φ_Sw2]
+@test state[2:3, :φ] ≈ [φ_Sw1, φ_Sw2]
+
+state_from_dict["bus2", :φ] = φ_Sw2
+@test state_from_dict[["bus2","bus3"], :φ] ≈ [φ_Sw2, φ_Sw2]
+state_from_dict[["bus2","bus3"], :φ] = [φ_Sw1, φ_Sw2]
+@test state_from_dict[["bus2","bus3"], :φ] ≈ [φ_Sw1, φ_Sw2]
 
 ## binary operations ##
 s1 = State(grid, ones(systemsize(grid)))
