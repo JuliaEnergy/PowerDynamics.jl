96b6befd26877b4f5ea1bf94b52fd4907c7e94ab
Hans Würfel <git@wuerfel.io>
2022-10-21 13:45:03 +0200

    check allocations in smoketest and warn (#205)

--- test/nodes/NodeTestBase.jl
+++ test/nodes/NodeTestBase.jl
@@ -1,14 +1,19 @@
-du,u,i = rand(ComplexF64, 3)
-t = rand(0.1:10.0)
-
 function smoketest_rhs(vertex; int_x=[], int_dx=[])
+    du,u,i = rand(ComplexF64, 3)
+    t = rand(0.1:10.0)
+
     dx = [real(du), imag(du)]
     x = [real(u), imag(u)]
     append!(x, int_x)
     append!(dx, int_dx)
     edges = [[real(i), imag(i), real(i), imag(i)]]
-    p = []
-    vertex.f(dx, x, edges, p, t)
+    p = nothing
+    vertex.f(dx, x, edges, p, t) # first call for precompilation
+
+    allocations = @allocated vertex.f(dx, x, edges, p, t)
+    if allocations != 0
+        @warn "RHS shows non-zero ($allocations) allocations!"
+    end
 end
 
 function rand_real()
