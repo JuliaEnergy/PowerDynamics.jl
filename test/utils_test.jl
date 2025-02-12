00eb698cff7461b5a7f76178f4d30ce71a2d5a27
Hans Würfel <git@wuerfel.io>
2025-02-12 18:55:04 +0100

    mv pin parameters test

--- test/utils_test.jl
+++ test/utils_test.jl
@@ -28,3 +28,42 @@ using ModelingToolkit: get_name
     @test ModelingToolkit.get_metadata(MyModel()) == (;origin="file", another=:foo)
     @test get_name(MyModel()) == :newname
 end
+
+@testset "pin parameters" begin
+    @mtkmodel Inner begin
+        @parameters begin
+            a = 0
+            b = 0
+        end
+        @variables begin
+            i(t)
+        end
+        @equations begin
+            i ~ a + b
+        end
+    end
+    @mtkmodel Outer begin
+        @components begin
+            inner = Inner()
+        end
+        @parameters begin
+            a = 0
+            b = 0
+        end
+        @variables begin
+            o(t)
+        end
+        @equations begin
+            o ~ inner.i * (a + b)
+        end
+    end
+    @named outer = Outer()
+    full_equations(outer)
+    sys1 = pin_parameters(outer, :a => 1)
+    sys2 = pin_parameters(outer, :outer₊b => 1)
+    full_equations(sys1)
+    full_equations(sys2)
+    sys1 = pin_parameters(outer, outer.inner.a => 1)
+    sys2 = pin_parameters(outer, :inner₊a => 1)
+    @test full_equations(sys1) == full_equations(sys2)
+end
