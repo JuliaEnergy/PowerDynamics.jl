0b907e62f08eecc5711d67d4bc35e6ba3fd3f415
Mandy Wältermann <mandy.waeltermann@tu-dortmund.de>
2025-02-19 10:17:08 +0100

    pQLoad conflict

--- src/Library/Loads/PQLoad.jl
+++ src/Library/Loads/PQLoad.jl
@@ -80,5 +80,35 @@ end
         P ~ terminal.u_r*terminal.i_r + terminal.u_i*terminal.i_i
         Q ~ terminal.u_i*terminal.i_r - terminal.u_r*terminal.i_i
     end
+end
 
+# TODO: S_b for loads?
+@mtkmodel ZIPLoad begin
+    @parameters begin
+        Pset, [description="Active Power at operation point [pu]"]
+        Qset, [description="Reactive Power at operation point [pu]"]
+        Vset, [guess=1,description="Voltage at operation point [pu]"]
+        KpZ, [description="Active power constant impedance fraction"]
+        KqZ, [description="Reactive power constant impedance fraction"]
+        KpI, [description="Active power constant current fraction"]
+        KqI, [description="Reactive power constant current fraction"]
+        KpC=1-KpZ-KpI, [description="Active power constant power fraction"]
+        KqC=1-KqZ-KqI, [description="Reactive power constant power fraction"]
+    end
+    @components begin
+        terminal = Terminal()
+    end
+    @variables begin
+        Vrel(t), [description="Relative voltage magnitude"]
+        P(t), [description="Active Power"]
+        Q(t), [description="Reactive Power"]
+    end
+    @equations begin
+        Vrel ~ sqrt(terminal.u_r^2 + terminal.u_i^2)/Vset
+        P ~ Pset*(KpZ*Vrel^2 + KpI*Vrel + KpC)
+        Q ~ Qset*(KqZ*Vrel^2 + KqI*Vrel + KqC)
+        # formulate equations for i_r and i_i instead
+        terminal.i_r ~  simplify(real((P + im*Q)/(terminal.u_r + im*terminal.u_i)))
+        terminal.i_i ~ -simplify(imag((P + im*Q)/(terminal.u_r + im*terminal.u_i)))
+    end
 end
