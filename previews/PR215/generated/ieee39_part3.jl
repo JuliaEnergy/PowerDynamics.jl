c4aa5e3df05f3a66d1bb74bc5246de25aabc344b
Documenter.jl <documenter@juliadocs.github.io>
2025-08-05 16:49:32 +0000

    build based on d247342

--- previews/PR215/generated/ieee39_part3.jl
+++ previews/PR215/generated/ieee39_part3.jl
@@ -1,6 +1,12 @@
 # # IEEE39 Bus Tutorial - Part III: Dynamic Simulation
 
 include("ieee39_part1.jl")
-nw
+# add additional init formulas as described in Part II
+formula = @initformula :ZIPLoad₊Vset = sqrt(:busbar₊u_r^2 + :busbar₊u_i^2)
+set_initformula!(nw[VIndex(31)], formula)
+set_initformula!(nw[VIndex(39)], formula)
+# find the initial state
+s0 = initialize_from_pf!(nw; verbose=false)
+nothing #hide
 
 # This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
