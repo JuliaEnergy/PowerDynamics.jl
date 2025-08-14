dbc07f7fa544b318ad3f3cf8069bbb6494cefe81
Hans Würfel <git@wuerfel.io>
2025-08-14 12:48:11 +0200

    update custom metadata handling

--- test/utils_test.jl
+++ test/utils_test.jl
@@ -3,6 +3,7 @@ using PowerDynamics.Library
 using ModelingToolkit
 using ModelingToolkit: t_nounits as t, D_nounits as Dt
 using ModelingToolkit: get_name
+using PowerDynamics: get_custom_metadata
 
 @info "Start Utils tests"
 
@@ -14,20 +15,20 @@ using ModelingToolkit: get_name
     end
     @test_throws UndefKeywordError MyModel()
     @test get_name(MyModel(name=:bar)) == :bar
-    @test ModelingToolkit.get_metadata(MyModel(name=:bar)) == nothing
+    default_metadata = ModelingToolkit.get_metadata(MyModel(name=:bar))
     # @b MyModel(name=:bar)
 
     @attach_metadata! MyModel (;origin="file", name=:defname)
-    @test ModelingToolkit.get_metadata(MyModel()) == (;origin="file")
+    @test get_custom_metadata(MyModel(), :origin) == "file"
     @test get_name(MyModel()) == :defname
     @test get_name(MyModel(name=:bar)) == :bar
     # @b MyModel()
 
     @attach_metadata! MyModel (;another=:foo)
-    @test ModelingToolkit.get_metadata(MyModel()) == (;origin="file", another=:foo)
+    @test get_custom_metadata(MyModel()) == (; origin = "file", another = :foo)
     @test get_name(MyModel()) == :defname
 
     @attach_metadata! MyModel (;name=:newname)
-    @test ModelingToolkit.get_metadata(MyModel()) == (;origin="file", another=:foo)
+    @test get_custom_metadata(MyModel()) == (; origin = "file", another = :foo)
     @test get_name(MyModel()) == :newname
 end
