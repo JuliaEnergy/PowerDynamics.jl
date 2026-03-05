f40ef8ceb64bb9d7162ba2fdbedefbc9bed6d546
Documenter.jl <documenter@juliadocs.github.io>
2026-03-05 21:45:28 +0000

    build based on 51a917a

--- previews/PR256/generated/linear_analysis.jl
+++ previews/PR256/generated/linear_analysis.jl
@@ -33,6 +33,7 @@ using ModelingToolkit: t_nounits as t, D_nounits as Dt
 using OrdinaryDiffEqRosenbrock
 using OrdinaryDiffEqNonlinearSolve
 using NetworkDynamics
+using NetworkDynamics: feedback
 using CairoMakie
 
 @mtkmodel SyncMachineStatorDynamics begin
